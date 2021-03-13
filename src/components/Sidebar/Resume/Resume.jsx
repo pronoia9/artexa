4d31230666f57fb62b8f273587a958f85688ef26
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-13 22:57:40 +0300

    refactor resume section in sidebar

--- src/components/Sidebar/Resume/Resume.jsx
+++ src/components/Sidebar/Resume/Resume.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import data from '../../../data/data.json';
 import './resume.css';
 
 // TODO: Add resume and replace the placeholder
@@ -6,7 +7,7 @@ import './resume.css';
 function Resume() {
   return (
     <div className='art-links-frame p-15-15'>
-      <a href='../../../variables/data.json' className='art-link' download>
+      <a href={data.resume} className='art-link' download>
         Download Resume <i className='fas fa-download'></i>
       </a>
     </div>
