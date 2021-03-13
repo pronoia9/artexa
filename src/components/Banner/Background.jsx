232d8c4e1bb88686b838ccc6cf77e4b7e6de31db
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-13 23:33:24 +0300

    remove images, fix scroll content padding

--- src/components/Banner/Background.jsx
+++ src/components/Banner/Background.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import data from '../../data/data.json';
 
 function Banner() {
   return (
     //top background
-    <div className='art-top-bg'>
+    <div className='art-top-bg' style={{backgroundImage: "url(" + data.background + ")"}}>
       {/* overlay */}
       <div className='art-top-bg-overlay'></div>
     </div>
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Code from './Code';
+import data from '../../data/data.json';
 import './banner.css';
 const pfp = require('../../assets/images/bitmoji/8b80b4d8-32db-4668-8490-9b1227b0754b-f0fe3384-3fbc-4f46-9738-7b713055c737-v1.png')
   .default;
@@ -11,7 +12,7 @@ function Banner() {
     <div className='container-fluid'>
       <div className='row p-60-0 p-lg-30-0 p-md-15-0'>
         <div className='col-lg-12'>
-          <div className='art-a art-banner'>
+          <div className='art-a art-banner' style={{ backgroundImage: 'url(' + data.background + ')' }}>
             <div className='art-banner-back'></div>
             <div className='art-banner-dec'></div>
             <div className='art-banner-overlay'>
