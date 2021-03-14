c00b1cc4c34760175987e5292b39416173f2558e
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-14 21:10:41 +0300

    change color theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Preloader from './components/Preloader/Preloader';
 import Wrapper from './Wrapper';
-import './assets/css/colors/pastels.css';
+import './assets/css/colors/yellow.css';
 import './assets/css/styles.css';
 
 export default function App() {
