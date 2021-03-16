8be5aa0f11cbcaca02099fe3810a49b790ea6aab
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-16 15:27:29 +0300

    leverage react context with formik

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useFormik } from 'formik';
+import React from 'react';
+import { useFormik, Formik } from 'formik';
 import * as Yup from 'yup';
 import { formButtonAnimation } from '../../assets/js/main.js';
 
@@ -31,78 +31,98 @@ export default function ContactForm(props) {
         </div>
       </div>
 
-      {/* form */}
+      {/* form container */}
       <div className='art-a art-card'>
-        <form id='form' className='art-contact-form' onSubmit={(e) => e.preventDefault()}>
-          {/* name */}
-          <div className='art-form-field'>
-            <input
-              id='name'
-              name='name'
-              className={'art-input' + (formik.values.name ? ' art-active' : '')}
-              type='text'
-              placeholder='Name'
-              {...formik.getFieldProps('name')}
-            ></input>
-            <label for='name'>
-              <i className='fas fa-user'></i>
-            </label>
-            <div className='art-validation-warning'>
-              {formik.touched.name && formik.errors.name ? <span>{formik.errors.name}</span> : null}
-            </div>
-          </div>
+        <Formik
+          initialValues={{ name: '', email: '', message: '' }}
+          validationSchema={Yup.object({
+            name: Yup.string().max(35, 'Must be 35 characters or less').required('Required'),
+            email: Yup.string().email('Invalid email address').required('Required'),
+            message: Yup.string().max(500, 'Must be 500 characters or less').required('Required'),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            // TODO: add post and remove alert
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+            formButtonAnimation();
+          }}
+        >
+          {/* form */}
+          {(formik) => (
+            <form id='form' className='art-contact-form' onSubmit={(e) => e.preventDefault()}>
+              {/* name */}
+              <div className='art-form-field'>
+                <input
+                  id='name'
+                  name='name'
+                  className={'art-input' + (formik.values.name ? ' art-active' : '')}
+                  type='text'
+                  placeholder='Name'
+                  {...formik.getFieldProps('name')}
+                ></input>
+                <label for='name'>
+                  <i className='fas fa-user'></i>
+                </label>
+                <div className='art-validation-warning'>
+                  {formik.touched.name && formik.errors.name ? <span>{formik.errors.name}</span> : null}
+                </div>
+              </div>
 
-          {/* email */}
-          <div className='art-form-field'>
-            <input
-              id='email'
-              name='email'
-              className={'art-input' + (formik.values.email ? ' art-active' : '')}
-              type='email'
-              placeholder='Email'
-              {...formik.getFieldProps('email')}
-            ></input>
-            <label for='email'>
-              <i className='fas fa-at'></i>
-            </label>
-            <div className='art-validation-warning'>
-              {formik.touched.email && formik.errors.email ? <span>{formik.errors.email}</span> : null}
-            </div>
-          </div>
+              {/* email */}
+              <div className='art-form-field'>
+                <input
+                  id='email'
+                  name='email'
+                  className={'art-input' + (formik.values.email ? ' art-active' : '')}
+                  type='email'
+                  placeholder='Email'
+                  {...formik.getFieldProps('email')}
+                ></input>
+                <label for='email'>
+                  <i className='fas fa-at'></i>
+                </label>
+                <div className='art-validation-warning'>
+                  {formik.touched.email && formik.errors.email ? <span>{formik.errors.email}</span> : null}
+                </div>
+              </div>
 
-          {/* message */}
-          <div className='art-form-field'>
-            <textarea
-              id='message'
-              name='message'
-              className={'art-input' + (formik.values.message ? ' art-active' : '')}
-              placeholder='Message'
-              {...formik.getFieldProps('message')}
-            ></textarea>
-            <label for='message'>
-              <i className='far fa-envelope'></i>
-            </label>
-            <div className='art-validation-warning art-message-validation'>
-              {formik.touched.message && formik.errors.message ? (
-                <span>{formik.errors.message}</span>
-              ) : null}
-            </div>
-          </div>
+              {/* message */}
+              <div className='art-form-field'>
+                <textarea
+                  id='message'
+                  name='message'
+                  className={'art-input' + (formik.values.message ? ' art-active' : '')}
+                  placeholder='Message'
+                  {...formik.getFieldProps('message')}
+                ></textarea>
+                <label for='message'>
+                  <i className='far fa-envelope'></i>
+                </label>
+                <div className='art-validation-warning art-message-validation'>
+                  {formik.touched.message && formik.errors.message ? (
+                    <span>{formik.errors.message}</span>
+                  ) : null}
+                </div>
+              </div>
 
-          {/* submit */}
-          <div className='art-submit-frame'>
-            <button
-              className='art-btn art-btn-md art-submit'
-              type='submit'
-              onClick={() => formik.handleSubmit()}
-            >
-              <span>Send Message</span>
-            </button>
-            <div className='art-success'>
-              Success <i className='fas fa-check'></i>
-            </div>
-          </div>
-        </form>
+              {/* submit */}
+              <div className='art-submit-frame'>
+                <button
+                  className='art-btn art-btn-md art-submit'
+                  type='submit'
+                  onClick={() => formik.handleSubmit()}
+                >
+                  <span>Send Message</span>
+                </button>
+                <div className='art-success'>
+                  Success <i className='fas fa-check'></i>
+                </div>
+              </div>
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
