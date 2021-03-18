bc24d228c8a70a2f148eb65bf06bc2111702c9fe
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-18 21:49:54 +0300

    remove unused files

--- src/components/Template/buttons/Link.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Link(props) {
-  return (
-    <a href={props.link} className={props.className} target={props.target}>
-      <span>{props.text}</span>
-    </a>
-  );
-}
