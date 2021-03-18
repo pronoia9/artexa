6e3b1a1c7be6e5f46587746a68eb0207894c5035
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-18 14:59:30 +0300

    add router and switch

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,66 +1,32 @@
 import React from 'react';
-import Scrollbar from 'smooth-scrollbar-react';
-import Background from '../components/Background';
 import Banner from '../components/Banner/Banner';
 import Counters from '../components/Counter/Counters';
 import History from '../components/History/History';
-import Projects from '../components/Projects/Projects';
-// import Test from './Test';
-import Contact from '../components/Contact/Contact';
+import Projects from '../components/Projects/ProjectsSwiper';
 import Blog from '../components/Blog/Blog';
-import Footer from '../components/Footer';
+import Contact from '../components/Contact/Contact';
 
 export default function HomePage(props) {
   return (
-    // activate/show curtain (css) if any of the panels are open/active
-    <div className={props.navbarActive || props.sidebarActive ? 'art-content art-active' : 'art-content'}>
-      {/* curtain */}
-      <div
-        className='art-curtain'
-        onClick={() => {
-          props.navbarActive && props.setNavbarActive(false);
-          props.sidebarActive && props.setSidebarActive(false);
-        }}
-      ></div>
+    <>
+      {/* heading for banner */}
+      <Banner background={props.background} setBackground={props.setBackground} data={props.data.banner} />
 
-      {/* send the background state from wrapper, which changes dynamically (on input) */}
-      <Background background={props.background} setBackground={props.setBackground} />
+      {/* counters */}
+      <Counters data={props.data.counters} />
 
-      {/* swup container */}
-      <div className='transition-fade'>
-        {/* scroll frame */}
-        <Scrollbar>
-          <div id='scrollbar' className='art-scroll-frame' data-scrollbar='true' tabIndex='-1'>
-            <div className='scroll-content'>
-              {/* heading for banner */}
-              <Banner
-                background={props.background}
-                setBackground={props.setBackground}
-                data={props.data.banner}
-              />
+      {/* education history */}
+      <History data={props.data.history} />
 
-              {/* counters */}
-              <Counters data={props.data.counters} />
+      {/* projects */}
+      <Projects data={props.data.projects} />
+      {/* <Test data={props.data.projects} /> */}
 
-              {/* education history */}
-              <History data={props.data.history} />
+      {/* newsletter / posts */}
+      <Blog data={props.data.blog} />
 
-              {/* projects */}
-              <Projects data={props.data.projects} />
-              {/* <Test data={props.data.projects} /> */}
-
-              {/* newsletter / posts */}
-              <Blog data={props.data.blog} />
-
-              {/* contact info */}
-              <Contact data={props.data.contact} />
-
-              {/* footer */}
-              <Footer data={props.data.logos} />
-            </div>
-          </div>
-        </Scrollbar>
-      </div>
-    </div>
+      {/* contact info */}
+      <Contact data={props.data.contact} />
+    </>
   );
 }
