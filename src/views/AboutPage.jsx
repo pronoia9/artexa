6b924f54753da388d65d1154117b3fc3dea8fd29
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-19 22:36:07 +0300

    change about page

--- src/views/AboutPage.jsx
+++ src/views/AboutPage.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
-import Banner from '../components/Banner/Banner';
 import About from '../components/About/About';
 
 export default function AboutPage(props) {
-  return (
-    <>
-      {/* heading for banner */}
-      <Banner background={props.background} setBackground={props.setBackground} data={props.data.banner} />
-
-      {/* personal statement */}
-      <About background={props.background} />
-    </>
-  );
+  return <About background={props.background} />;
 }
