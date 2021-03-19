989a15c38a01be7d15626138dd76f171eea4b7c4
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-19 20:26:35 +0300

    minor adjustments

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner';
 import Counters from '../components/Counter/Counters';
-import About from '../components/About/About';
 import History from '../components/History/History';
 import ProjectsSwiper from '../components/Projects/ProjectsSwiper';
 import BlogSwiper from '../components/Blog/BlogSwiper';
@@ -16,15 +15,11 @@ export default function HomePage(props) {
       {/* counters */}
       <Counters data={props.data.counters} />
 
-      {/* personal statement */}
-      <About background={props.background} />
+      {/* projects */}
+      <ProjectsSwiper data={props.data.projects} />
 
       {/* education history */}
       <History data={props.data.history} />
-
-      {/* projects */}
-      <ProjectsSwiper data={props.data.projects} />
-      {/* <Test data={props.data.projects} /> */}
 
       {/* newsletter / posts */}
       <BlogSwiper data={props.data.blog} />
