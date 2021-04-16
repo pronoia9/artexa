da74589480c58b90bbaacd37884cac9becbd404b
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-04-16 15:36:07 +0300

    remove unnecessary files, replace resume

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Preloader from './components/Preloader/Preloader';
-// fancybox
-import './assets/css/fancybox.min.css';
 // swiper
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
@@ -24,8 +21,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
