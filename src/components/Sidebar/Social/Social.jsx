ec7e514adab39d49940b75e9c2bbd40558dfbd89
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-28 17:56:03 -0400

    fix fancybox url (if there is fancybox)

--- src/components/Sidebar/Social/Social.jsx
+++ src/components/Sidebar/Social/Social.jsx
@@ -5,7 +5,7 @@ export default function Social(props) {
   return (
     <div className='art-ls-social'>
       {props.social.map((s) => (
-        <SocialIcon key={s.id} platform={s.platform} url={s.url} favicon={s.favicon} fancybox={s.fancybox} />
+        <SocialIcon key={s.id} platform={s.platform} url={(s.fancybox ? process.env.PUBLIC_URL: '') + s.url} favicon={s.favicon} fancybox={s.fancybox} />
       ))}
     </div>
   );
