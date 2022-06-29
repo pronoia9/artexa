01b9ae39931bd1ac7454ae723a7c5ec1e75d468c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-28 21:36:41 -0400

    remove useParams + use Navigate comp on '/'

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Routes, Route, useParams, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 // pages
 import HomePage from '../views/HomePage';
 import ProjectsPage from '../views/ProjectsPage';
@@ -16,19 +16,19 @@ export default function App({ data, background, changeBackground }) {
 
   return (
     <Routes>
-      <Route exact path='/projects'           element={<ProjectsPage data={projects} />} />
-      <Route exact path='/projects-2-col'     element={<ProjectsPage data={projects} column={2} />} />
-      <Route exact path='/projects-3-col'     element={<ProjectsPage data={projects} column={3} />} />
-      <Route exact path='/history'            element={<HistoryPage data={history} />} />
-      <Route exact path='/courses'            element={<CoursePage data={courses} />} />
-      <Route exact path='/technologies'       element={<BlogPage data={technologies} />} />
+      <Route exact path='/projects' element={<ProjectsPage data={projects} />} />
+      <Route exact path='/projects-2-col' element={<ProjectsPage data={projects} column={2} />} />
+      <Route exact path='/projects-3-col' element={<ProjectsPage data={projects} column={3} />} />
+      <Route exact path='/history' element={<HistoryPage data={history} />} />
+      <Route exact path='/courses' element={<CoursePage data={courses} />} />
+      <Route exact path='/technologies' element={<BlogPage data={technologies} />} />
       <Route exact path='/technologies-2-col' element={<BlogPage data={technologies} column={2} />} />
       <Route exact path='/technologies-3-col' element={<BlogPage data={technologies} column={3} />} />
-      <Route exact path='/about'              element={<AboutPage hobbies={hobbies} />} />
-      <Route exact path='/contact'            element={<ContactPage data={contact} />} />
-      <Route exact path='/portfolio'          element={<HomePage data={data} background={background} changeBackground={changeBackground} />} />
-      <Route exact path='/'                   element={<HomePage data={data} background={background} changeBackground={changeBackground} />} />
-      <Route path='*'                         element={<NotFound />} />
+      <Route exact path='/about' element={<AboutPage hobbies={hobbies} />} />
+      <Route exact path='/contact' element={<ContactPage data={contact} />} />
+      <Route exact path='/portfolio' element={<HomePage data={data} background={background} changeBackground={changeBackground} />} />
+      <Route exact path='/' element={<Navigate to='/portfolio' />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
