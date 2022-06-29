59c8c45aedc6bf277e2ce2a9c9a4d57a4f787494
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-29 14:11:37 -0400

    object destructuring + check on props

--- src/components/Technologies/TechnologiesCard.jsx
+++ src/components/Technologies/TechnologiesCard.jsx
@@ -1,24 +1,27 @@
-export default function TechnologiesCard(props) {
-  return (
+export default function TechnologiesCard({ item, message }) {
+  const { id, filters, url, title, tags, images, overview } = item;
+  return id && filters && url && title.length && tags && images ? (
     <div className='art-a art-blog-card'>
       {/* post cover */}
-      <a data-fancybox='posts' href={process.env.PUBLIC_URL + props.post.image} className='art-port-cover'>
-        <img src={process.env.PUBLIC_URL + props.post.image} alt='post' />
+      <a data-fancybox='posts' href={process.env.PUBLIC_URL + images[0]} className='art-port-cover'>
+        <img src={process.env.PUBLIC_URL + images[0]} alt='post' />
       </a>
 
       {/* post description */}
       <div className='art-post-description'>
         {/* title */}
-        <a href={props.post.url} target='_blank' rel='noreferrer'>
-          <h5 className='mb-15'>{props.post.title}</h5>
+        <a href={url} target='_blank' rel='noreferrer'>
+          <h5 className='mb-15'>{title}</h5>
         </a>
         {/* text */}
-        <div className='mb-15'>{props.post.overview}</div>
+        <div className='mb-15'>{overview}</div>
         {/* link */}
-        <a href={props.post.url} className='art-link art-color-link art-w-chevron' target='_blank' rel='noreferrer'>
-          {props.message}
+        <a href={url} className='art-link art-color-link art-w-chevron' target='_blank' rel='noreferrer'>
+          {message}
         </a>
       </div>
     </div>
+  ) : (
+    <></>
   );
 }
