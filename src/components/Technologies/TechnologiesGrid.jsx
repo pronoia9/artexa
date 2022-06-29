ddcbdcde896b0601895c63d22922e8526aea573b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-29 12:49:15 -0400

    object destructuring

--- src/components/Technologies/TechnologiesGrid.jsx
+++ src/components/Technologies/TechnologiesGrid.jsx
@@ -1,17 +1,18 @@
 import TechnologiesCard from './TechnologiesCard';
 import SectionTitle from '../Templates/Text/SectionTitle';
 
-export default function TechnologiesGrid(props) {
+export default function TechnologiesGrid({ data, column }) {
+  const { title, message, packages } = data;
   return (
     <div className='container-fluid'>
       <div className='row p-30-0'>
         {/* heading */}
-        <SectionTitle title={props.data.title} />
+        <SectionTitle title={title} />
 
         {/* content */}
-        {props.data.posts.map((item) => (
-          <div className={'col-lg-' + 12 / props.column}>
-            <TechnologiesCard key={item.id} post={item} message={props.data.message} />
+        {packages.map((item) => (
+          <div className={'col-lg-' + 12 / column}>
+            <TechnologiesCard key={item.id} post={item} message={message} />
           </div>
         ))}
       </div>
