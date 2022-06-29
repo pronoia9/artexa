32c567a0cf284905eecc2407fc33960b56fb58c8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-29 12:05:03 -0400

    add currentPath as prop

--- src/components/Navbar/NavbarList.jsx
+++ src/components/Navbar/NavbarList.jsx
@@ -1,18 +1,12 @@
 import NavbarListItem from './NavbarListItem';
 
-export default function NavbarList({ data, setNavbarActive, currentPage, setCurrentPage }) {
+export default function NavbarList({ data, setNavbarActive, currentPage, currentPath }) {
   return (
     <div className='art-scroll-frame'>
       <nav>
         <ul className='main-menu'>
           {data.map((li) => (
-            <NavbarListItem
-              key={li.title}
-              item={li}
-              setNavbarActive={setNavbarActive}
-              currentPage={currentPage}
-              setCurrentPage={setCurrentPage}
-            />
+            <NavbarListItem key={li.title} item={li} setNavbarActive={setNavbarActive} currentPage={currentPage} currentPath={currentPath} />
           ))}
         </ul>
       </nav>
