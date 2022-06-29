e4edfa41c7971d9bdb1903c83d6bafbf8cdb4acb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-29 15:45:38 -0400

    update counters

--- src/components/Counter/Counters.jsx
+++ src/components/Counter/Counters.jsx
@@ -2,15 +2,16 @@ import React, { useEffect } from 'react';
 import Counter from './Counter';
 import { counterAnimation } from '../../assets/js/main.js';
 
-export default function Counters(props) {
+export default function Counters({ data, projectsCount }) {
+  console.log(projectsCount);
   // animation (fade-in & count increase)
   useEffect(counterAnimation, []);
 
   return (
     <div className='container-fluid'>
       <div className='row p-30-0'>
-        {props.data.map((c) => (
-          <Counter key={c.id} id={c.id} length={props.data.length} number={c.number} text={c.text} />
+        {data.map((c) => (
+          <Counter key={c.id} id={c.id} length={data.length} number={c.text === 'Completed Projects' ? projectsCount : c.text === 'Years Experience' ? (new Date().getFullYear() -  c.number + 1) : c.number} text={c.text} />
         ))}
       </div>
     </div>
