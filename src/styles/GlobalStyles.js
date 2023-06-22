d3482e3600b739e90eb6a03932cca4c4c06ab15f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 02:20:14 +0300

    update gradients

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -18,39 +18,20 @@ const GlobalStyles = createGlobalStyle`
     --c-font-6: ${({ theme }) => theme.font6};
     --c-accent-1: ${({ theme }) => theme.accent1};
     --c-accent-2: ${({ theme }) => theme.accent2};
-    --c-accent-3: ${({ theme }) => theme.accent3};
     --c-accent-4: ${({ theme }) => theme.accent4};
     --c-accent-5: ${({ theme }) => theme.accent5};
-    --c-gradient: linear-gradient(0.25turn, var(--c-primary-1), var(--c-primary-2), var(--c-primary-3), var(--c-primary-4));
     --c-gradient-1: ${({ theme }) => theme.gradient1};
     --c-gradient-2: ${({ theme }) => theme.gradient2};
-    --c-gradient-3: ${({ theme }) => theme.gradient3};
-    --c-gradient-4: ${({ theme }) => theme.gradient4};
-    --c-gradient-5: ${({ theme }) => theme.gradient5};
-    --c-gradient-6: ${({ theme }) => theme.gradient6};
-    --c-gradient-7: ${({ theme }) => theme.gradient7};
-    --c-gradient-8: ${({ theme }) => theme.gradient8};
-    --c-gradient-9: ${({ theme }) => theme.gradient9};
-    --c-gradient-10: ${({ theme }) => theme.gradient10};
+    --c-gradient-3: ${({ theme }) => theme.gradient4};
+    --c-gradient-4: ${({ theme }) => theme.gradient5};
+    --c-gradient-5: ${({ theme }) => theme.gradient6};
+    --c-gradient-6: ${({ theme }) => theme.gradient7};
+    --c-gradient-7: ${({ theme }) => theme.gradient8};
+    --c-gradient-8: ${({ theme }) => theme.gradient10};
+    --c-gradient-9: linear-gradient(0.25turn, var(--c-primary-2), var(--c-primary-3), var(--c-primary-4), var(--c-primary-5));
     --c-cursor: ${({ theme }) => theme.accent - 1};
     --f-primary: 'Poppins', sans-serif;
     --f-code: var(--f-code);
-    --color-text-0: #fafafc;
-	  --color-text-1: #8c8c8e;
-	  --color-text-2: #646466;
-	  --color-text-3: #20202a;
-	  --color-text-4: #1e1e28;
-	  --color-text-5: #121217;
-	  --color-background-0: #191923;
-	  --color-background-1: #1e1e28e0;
-	  --color-box-shadow-0: #0f0f1433;
-	  --color-text-shadow-0: #fafafc66;
-	  --color-gradient-1-1: #1e1e28ed;
-	  --color-gradient-1-2: #1e1e28f5;
-	  --color-gradient-1-3: #1e1e28fc;
-	  --color-gradient-1-4: #1e1e28;
-	  --color-gradient-2-1: #252532fa;
-	  --color-gradient-2-2: #23232dfa;
   }
 
   *::-webkit-scrollbar {
