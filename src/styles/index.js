de2433ec913edeb1519febec7f8925b5e1647dac
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 02:20:07 +0300

    remove css

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,15 +1,4 @@
-// CSS
-import { normalize } from './NormalizeCSS';
-import { reset } from './ResetCSS';
-// Themes
 import GlobalStyles from './GlobalStyles';
 import { themes, colors } from './Themes';
-// Styles
 
-export {
-  // CSS
-  normalize, reset,
-  // Themes
-  GlobalStyles, themes, colors,
-  // Styles
-}
+export { GlobalStyles, themes, colors };
