b8668e0f732e5d5b6e15a17af32b8fd1b834bc63
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-22 23:34:12 +0300

    add footer images

---
+++ src/assets/footer/index.js
@@ -0,0 +1,6 @@
+import footer1 from './friend of the year.png';
+import footer2 from './I did it.png';
+import footer3 from './sticker-2.png';
+import footer4 from './sticker.png';
+
+export { footer1, footer2, footer3, footer4 };
---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+// Footer
+import { footer1, footer2, footer3, footer4 } from './footer';
+// Projects
+
+export {
+  // Footer
+  footer1, footer2, footer3, footer4,
+  // Projects
+}
