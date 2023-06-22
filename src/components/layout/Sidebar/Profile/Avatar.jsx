4611cfae7b90970d5e25f5e5b52ad45fc23642a1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 00:18:06 +0300

    finish avatar

--- src/components/layout/Sidebar/Profile/Avatar.jsx
+++ src/components/layout/Sidebar/Profile/Avatar.jsx
@@ -1,4 +1,19 @@
+import { dataStore } from '../../../../store/dataStore';
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  const data = dataStore((state) => state.sidebar.profile);
+
+  return (
+    <div className='art-avatar'>
+      <a data-fancybox='avatar' href={data.avatar} className='art-avatar-curtain'>
+        <img src={data.avatar} alt='Avatar' />
+        <i className='fas fa-expand' />
+      </a>
+      {/* availability: change class 'art-not-available' if not available */}
+      <div className={`art-lamp-light${data.available ? '' : ' art-not-available'}`}>
+        <div className='art-available-lamp' />
+      </div>
+    </div>
+  );
 };
 export default Avatar;
