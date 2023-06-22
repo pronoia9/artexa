2e75de56352d942bc964f5a6d9804654160b6741
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-22 16:59:08 +0300

    init utils + motion

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+// Framer Motion
+// Utils
+import { getSystemTheme, getTheme, isDarkTheme, systemThemeChangeHandler, lowerCase, rem } from './utils';
+
+export {
+  // Framer Motion
+  // Utils
+  getSystemTheme, getTheme, isDarkTheme, systemThemeChangeHandler, lowerCase, rem
+}
