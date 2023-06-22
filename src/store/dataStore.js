46864e7675da6bc8d508e9b07e0e658c3d449654
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 00:14:58 +0300

    add init sidebar data

--- src/store/dataStore.js
+++ src/store/dataStore.js
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-import { footer1, footer2, footer3, footer4 } from '../assets';
+import { avatar, footer1, footer2, footer3, footer4 } from '../assets';
 import { getSystemTheme } from '../utils';
 
 export const dataStore = create((set) => ({
@@ -19,12 +19,108 @@ export const dataStore = create((set) => ({
   openNavbar: () => set({ navbarOpen: true, sidebarOpen: false }),
   sidebarOpen: true,
   openSidebar: () => set({ navbarOpen: false, sidebarOpen: true }),
-  toggleSidebar: () => set((state) => ({
-    navbarOpen: navbarOpen && !sidebarOpen ? false : state.navbarOpen,
-    sidebarOpen: !state.sidebarOpen,
-  })),
-  
+  toggleSidebar: () =>
+    set((state) => ({
+      navbarOpen: navbarOpen && !sidebarOpen ? false : state.navbarOpen,
+      sidebarOpen: !state.sidebarOpen,
+    })),
+
   // SIDEBAR
+  sidebar: {
+    profile: {
+      name: 'Jay Ansin',
+      subtitle: ['Full-Stack Developer,', 'Avid pet lover 🐶'],
+      available: true,
+      avatar,
+    },
+
+    about: [
+      { id: 1, title: 'Nationality:', subtitle: 'U.S.' },
+      { id: 2, title: 'Age', subtitle: new Date().getFullYear() - new Date('April 15, 1995').getFullYear() },
+    ],
+
+    languages: [
+      { id: '1', language: 'English', level: 100 },
+      { id: '2', language: 'French', level: 10 },
+      { id: '2', language: 'Japanese', level: 5 },
+    ],
+
+    hardSkills: [
+      { id: '1', skill: 'HTML5', level: 85 },
+      { id: '2', skill: 'CSS3', level: 60 },
+      { id: '3', skill: 'JavaScript', level: 80 },
+      { id: '4', skill: 'ReactJS', level: 70 },
+      { id: '5', skill: 'NodeJS', level: 50 },
+    ],
+    
+    softSkills: [
+      'Nextjs, Redux',
+      'MySQL, MongoDB, PostgresSQL',
+      'Blender, Threejs',
+      'Git, GitHub',
+      'Bash, Zsh',
+      'Photoshop, Excel',
+    ],
+
+    strengths: [
+      'Communicating',
+      'Team oriented',
+      'Organizational',
+      'Logical thinking',
+      'Detail oriented',
+      'Flexible',
+      'Caring',
+      'Dedicated',
+      'Punctual',
+      'Focused',
+      'Loyal',
+      'Adaptable',
+      'Tech savvy',
+    ],
+
+    resume: {
+      title: 'Download Resume',
+      url: '',
+    },
+
+    social: [
+      {
+        id: 1,
+        platform: 'github',
+        favicon: 'fab fa-github-square',
+        fancybox: null,
+        url: 'https://github.com/pronoia9/',
+      },
+      {
+        id: 2,
+        platform: 'linkedin',
+        favicon: 'fab fa-linkedin',
+        fancybox: null,
+        url: 'https://www.linkedin.com/in/jayansin/',
+      },
+      {
+        id: 3,
+        platform: 'facebook social-unavailable',
+        favicon: 'fab fa-facebook-square',
+        fancybox: null,
+        url: 'https://www.facebook.com/',
+      },
+      {
+        id: 4,
+        platform: 'snapchat',
+        favicon: 'fab fa-snapchat-square',
+        fancybox: 'snapchat',
+        url: '/images/bitmoji/snapchat.png',
+      },
+      {
+        id: 5,
+        platform: 'discord social-unavailable',
+        favicon: 'fab fa-discord',
+        fancybox: null,
+        url: 'https://discordapp.com/',
+      },
+    ],
+  },
 
   // NAVBAR
 
