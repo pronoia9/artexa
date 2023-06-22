85b3fdf4a8e508ecdff1eb65562dee2a0f689bbc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-22 23:49:19 +0300

    finish footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,4 +1,19 @@
+import { Logos } from '../..';
+import { dataStore } from '../../../store/dataStore';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const data = dataStore((state) => state.footer);
+
+  return (
+    <>
+      <Logos logos={data.logos} />
+      <div className='container-fluid'>
+        <footer>
+          <div>{data.text[0]}</div>
+          <div>{data.text[1]}</div>
+        </footer>
+      </div>
+    </>
+  );
 };
 export default Footer;
