79a4623c335753011dc97fd75775caeaa98f99b8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 00:20:45 +0300

    finish name

--- src/components/layout/Sidebar/Profile/Name.jsx
+++ src/components/layout/Sidebar/Profile/Name.jsx
@@ -1,4 +1,12 @@
+import { dataStore } from '../../../../store/dataStore';
+
 const Name = () => {
-  return <div>Name</div>;
+  const data = dataStore((state) => state.sidebar.profile);
+
+  return (
+    <h5 className='art-name mb-10'>
+      <a href='/'>{data.name}</a>
+    </h5>
+  );
 };
 export default Name;
