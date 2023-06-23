12bbbe99743ea956b5b857973cead1ce13918869
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 17:53:27 +0300

    add styled components

--- src/components/layout/Sidebar/Profile/Profile.jsx
+++ src/components/layout/Sidebar/Profile/Profile.jsx
@@ -1,12 +1,26 @@
+import { styled } from 'styled-components';
 import { Avatar, Name, Post } from '.';
 
 const Profile = () => {
   return (
-    <div className='art-header'>
+    <Container className='art-header'>
       <Avatar />
       <Name />
       <Post />
-    </div>
+    </Container>
   );
 };
 export default Profile;
+
+const Container = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 99999;
+  width: 100%;
+  padding: 30px;
+  height: 235px;
+  background: var(--c-gradient-2);
+  text-align: center;
+  box-shadow: 0 1px 4px 0 var(--c-box-shadow);
+`;
