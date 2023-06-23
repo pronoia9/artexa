3bf92a80e23034f57074a3b0a744e5c9c03d05b4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 23:37:06 +0300

    add init navbar

--- src/components/layout/Navbar/NavbarList.jsx
+++ src/components/layout/Navbar/NavbarList.jsx
@@ -1,4 +1,18 @@
+import { dataStore } from '../../../store/dataStore';
+import { NavbarListItem } from './';
+
 const NavbarList = () => {
-  return <div>NavbarList</div>;
+  const data = dataStore((state) => state.routes);
+  return (
+    <div className='art-scroll-frame'>
+      <nav>
+        <ul className='main-menu'>
+          {data.map((route, index) => (
+            <NavbarListItem key={`navbarlist-${index}`} {...route} />
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 };
 export default NavbarList;
