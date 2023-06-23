ebe0da382e6242320dafa759424aba23b5973b6f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 19:12:36 +0300

    finish progress circle

--- src/components/components/Progress/ProgressCircle.jsx
+++ src/components/components/Progress/ProgressCircle.jsx
@@ -1,4 +1,20 @@
-const ProgressCircle = () => {
-  return <div>ProgressCircle</div>;
+import { useEffect } from 'react';
+import * as ProgressBar from 'progressbar.js';
+
+const ProgressCircle = ({ ids, classNames, percentage, index }) => {
+  useEffect(() => {
+    const circle = new ProgressBar.Circle(`#${ids}`, {
+      strokeWidth: 7,
+      easing: 'easeInOut',
+      duration: 1400,
+      delay: 2400 + Number(index) * 100,
+      trailWidth: 7,
+      step: (state, bar) => { bar.setText(percentage); },
+    });
+    circle.animate(percentage / 100);
+    return () => { circle.destroy(); };
+  }, []);
+
+  return <div id={ids} className={classNames} />;
 };
 export default ProgressCircle;
