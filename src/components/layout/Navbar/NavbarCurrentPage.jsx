207d58edff2643d3945f97407773d0c1c0a1a15c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 23:33:20 +0300

    finish init navbar current page

--- src/components/layout/Navbar/NavbarCurrentPage.jsx
+++ src/components/layout/Navbar/NavbarCurrentPage.jsx
@@ -1,4 +1,14 @@
+import { useLocation } from 'react-router-dom';
+import { dataStore } from '../../../store/dataStore';
+
 const NavbarCurrentPage = () => {
-  return <div>NavbarCurrentPage</div>;
+  const routes = dataStore((state) => state.routes);
+  let location = useLocation();
+  
+  return (
+    <div className='art-current-page'>
+      <span>{routes.find((r) => r.path === location.pathname)?.title}</span>
+    </div>
+  );
 };
 export default NavbarCurrentPage;
