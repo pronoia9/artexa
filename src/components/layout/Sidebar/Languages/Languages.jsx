a498f5b45a9e273331946daae452eedd938945ef
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 18:37:00 +0300

    change key

--- src/components/layout/Sidebar/Languages/Languages.jsx
+++ src/components/layout/Sidebar/Languages/Languages.jsx
@@ -1,4 +1,14 @@
+import { LanguagesItem } from '../..';
+import { dataStore } from '../../../../store/dataStore';
+
 const Languages = () => {
-  return <div>Languages</div>;
+  const data = dataStore((state) => state.sidebar.languages);
+  return (
+    <div className='art-lang-skills p-30-15'>
+      {data.map((language, index) => (
+        <LanguagesItem key={`sidebar-language-${index}`} {...language} />
+      ))}
+    </div>
+  );
 };
 export default Languages;
