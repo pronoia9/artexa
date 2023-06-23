3923722ee1bccc43963f97017595819898a4c657
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 23:22:55 +0300

    add init navbar

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,4 +1,17 @@
+import { NavbarButton, NavbarCurrentPage, NavbarList } from './';
+import { dataStore } from '../../../store/dataStore';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { navbarOpen } = dataStore((state) => ({ navbarOpen: state.navbarOpen }));
+
+  return (
+    <div className={`art-menu-bar${navbarOpen ? ' art-active' : ''}`} active={`${navbarOpen}`}>
+      <div id='art-menu-bar-frame' className='art-menu-bar-frame'>
+        <NavbarButton />
+        <NavbarCurrentPage />
+        <NavbarList />
+      </div>
+    </div>
+  );
 };
 export default Navbar;
