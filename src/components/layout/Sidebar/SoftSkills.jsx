353242309e978a3d13bf2b968f01056da736cb7c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 22:39:57 +0300

    finish soft skills

--- src/components/layout/Sidebar/SoftSkills.jsx
+++ src/components/layout/Sidebar/SoftSkills.jsx
@@ -1,8 +1,12 @@
-export const SoftSkills = () => {
-  return <div>SoftSkills</div>;
-};
-export default SoftSkills;
+import { dataStore } from '../../../store/dataStore';
 
-export const SoftSkillsItem = () => {
-  return <div>SoftSkillsItem</div>;
+export const SoftSkills = () => {
+  const data = dataStore((state) => state.sidebar.softSkills);
+  return (
+    <ul className='art-knowledge-list p-15-0'>
+      {data.map((skills, index) => (
+        <li key={`sidebar-softskills-${index}`}>{skills}</li>
+      ))}
+    </ul>
+  );
 };
