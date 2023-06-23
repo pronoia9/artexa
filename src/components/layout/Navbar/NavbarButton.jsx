c5f29f8302e184177c42d12b9e5a8f2831bdd06c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 23:28:33 +0300

    finish init navbar button

--- src/components/layout/Navbar/NavbarButton.jsx
+++ src/components/layout/Navbar/NavbarButton.jsx
@@ -1,4 +1,14 @@
+import { dataStore } from '../../../store/dataStore';
+
 const NavbarButton = () => {
-  return <div>NavbarButton</div>;
+  const { navbarOpen, toggleNavbar } = dataStore((state) => ({ navbarOpen: state.navbarOpen, toggleNavbar: state.toggleNavbar }));
+
+  return (
+    <div className='art-menu-bar-header'>
+      <div className={`art-menu-bar-btn${navbarOpen ? ' art-active' : ''}`} active={`${navbarOpen}`} onClick={() => toggleNavbar()}>
+        <span />
+      </div>
+    </div>
+  );
 };
 export default NavbarButton;
