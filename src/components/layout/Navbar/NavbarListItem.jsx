9fd6e1bcfadfeacab750011b6320ac3d00b4e367
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-24 00:01:01 +0300

    finish init navbar list item

--- src/components/layout/Navbar/NavbarListItem.jsx
+++ src/components/layout/Navbar/NavbarListItem.jsx
@@ -1,4 +1,27 @@
-const NavbarListItem = () => {
-  return <div>NavbarListItem</div>;
+import { useLocation, Link } from 'react-router-dom';
+import { dataStore } from '../../../store/dataStore';
+
+const NavbarListItem = ({ title, path, submenu }) => {
+  const closeNavbar = dataStore((state) => state.closeNavbar);
+  let location = useLocation();
+
+  const handleClick = (e) => {
+    if (location.pathname === path) e.preventDefault();
+    closeNavbar();
+  };
+
+  return (
+    <li
+      className={`menu-item${submenu ? ' menu-item-has-children' : ''}${
+        location.pathname === path ? (submenu ? ' current-menu-item' : ' current_page') : ''
+      }`}
+    >
+      <Link to={path} onClick={handleClick}>
+        {submenu
+          ? submenu.length && submenu.map((li) => <NavbarListItem key={`navbarlist-${title}-${index}`} {...li} />)
+          : <span>{title}</span>}
+      </Link>
+    </li>
+  );
 };
 export default NavbarListItem;
