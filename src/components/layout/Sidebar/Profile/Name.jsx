50a9957d7cac0cc183d9feafb2c1d53d8d7b3d33
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-23 18:02:26 +0300

    add styled components

--- src/components/layout/Sidebar/Profile/Name.jsx
+++ src/components/layout/Sidebar/Profile/Name.jsx
@@ -1,12 +1,24 @@
+import { styled } from 'styled-components';
 import { dataStore } from '../../../../store/dataStore';
 
 const Name = () => {
   const data = dataStore((state) => state.sidebar.profile);
 
   return (
-    <h5 className='art-name mb-10'>
-      <a href='/'>{data.name}</a>
-    </h5>
+    <Text className='art-name mb-10'>
+      <a href={data.nameLink}>{data.name}</a>
+    </Text>
   );
 };
 export default Name;
+
+const Text = styled.h5`
+  a {
+    color: var(--c-font-1);
+    transition: 0.4s ease-in-out;
+
+    &:hover {
+      color: var(--c-accent-1);
+    }
+  }
+`;
