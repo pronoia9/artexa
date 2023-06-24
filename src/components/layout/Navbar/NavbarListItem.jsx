9e81bfaaf7819f6c356945f5758c618a4a4fc8ea
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-24 16:44:22 +0300

    optimize styled components

--- src/components/layout/Navbar/NavbarListItem.jsx
+++ src/components/layout/Navbar/NavbarListItem.jsx
@@ -5,13 +5,13 @@ import { dataStore } from '../../../store/dataStore';
 import { rem } from '../../../utils';
 import { useState } from 'react';
 
-const NavbarListItem = ({ title, path, index, subMenu }) => {
+const NavbarListItem = ({ title, path, index, submenu }) => {
   const { navbarOpen, closeNavbar } = dataStore((state) => ({ navbarOpen: state.navbarOpen, closeNavbar: state.closeNavbar }));
   let location = useLocation();
   const [submenuOpen, setSubMenuOpen] = useState(false);
 
   const handleClick = (e) => {
-    if (subMenu) setSubMenuOpen((prev) => !prev);
+    if (submenu) setSubMenuOpen((prev) => !prev);
     else {
       if (location.pathname === path) e.preventDefault();
       closeNavbar();
@@ -20,17 +20,18 @@ const NavbarListItem = ({ title, path, index, subMenu }) => {
 
   return (
     <ListItem
-      className={`menu-item ${subMenu ? ' menu-item-has-children' : ''}`}
+      className={`menu-item ${submenu ? ' menu-item-has-children' : ''}`}
       $index={index + 1}
       $currentPage={location.pathname === path}
       $navbarOpen={navbarOpen}
+      $submenuopen={submenu && submenuOpen}
     >
-      {subMenu ? (
+      {submenu ? (
         <>
           <p onClick={handleClick}>{title}</p>
-          <ul className={`sub-menu ${submenuOpen ? ' art-active' : ''}`} $submenuOpen={submenuOpen}>
-            {subMenu.map((li, index) => (
-              <NavbarListItem key={`navbarlist-submenu-${index}`} {...li} />
+          <ul className='sub-menu'>
+            {submenu.map((li, index) => (
+              <NavbarListItem key={`navbarlist-${title}-${index}`} {...li} />
             ))}
           </ul>
           <i className='fa-solid fa-chevron-right' />
@@ -53,7 +54,7 @@ const ListItem = styled.li`
   opacity: ${({ $navbarOpen }) => ($navbarOpen ? 1 : 0)};
   transform: ${({ $navbarOpen }) => ($navbarOpen ? 'translateX(0)' : `translateX(${rem(60)})`)};
   transition: 0.55s ease-in-out;
-  ${({ $index }) => css`transition-delay: ${$index * 0.05}s;`};
+  ${({ $index }) =>css` transition-delay: ${$index * 0.05}s;`};
 
   * {
     color: ${({ $currentPage }) => ($currentPage ? 'var(--c-font-1)' : 'var(--c-font-2)')};
@@ -101,68 +102,29 @@ const ListItem = styled.li`
     position: relative;
     padding-left: 0;
     padding-right: 0;
-    
-    &:hover {
-      i {
-        /* color: var(--c-font-1); */
-        /* text-shadow: 0 0 ${rem(3)} var(--c-font-shadow); */
-        transform: rotate(90deg);
-      }
-    } 
-    
+
+    &:hover { i { transform: rotate(90deg); } }
+
     .sub-menu {
-      padding-left: 0;
-      width: 100%;
-      display: block;
       position: relative;
-      overflow: hidden;
-      pointer-events: none;
-      box-shadow: inset 0 3px 8px 0 var(--color-box-shadow-0);
-      background: var(--color-text-3);
+      width: 100%;
+      max-height: ${({ $submenuopen }) => $submenuopen ? '500px' : 0};
+      padding-left: 0;
+      display: block;
+      box-shadow: inset 0 3px 8px 0 var(--c-box-shadow);
+      background: var(--c-background-1);
       filter: brightness(95%);
-      max-height: 0;
+      pointer-events: ${({ $submenuopen }) => $submenuopen ? 'all' : 'none'};
+      overflow: hidden;
       transition: max-height 0.6s ease-in-out;
-  
+
       .menu-item {
-        position: relative;
-        opacity: 0;
-        transition: 0.55s ease-in-out;
-  
+        opacity: ${({$submenuopen }) => $submenuopen ? 1 : 0};
+
         a {
-          padding: 7px 30px;
-          font-weight: 400;
-          font-size: 10px;
-        }
-  
-        .sub-menu {
-          margin: 0;
-          max-height: 0;
-          transition: max-height 1s ease-in-out;
-        }
-  
-        &:hover {
-          &.menu-item-has-children:after {
-            transform: rotate(90deg);
-          }
-  
-          .sub-menu {
-            max-height: 500px;
-          }
-        }
-  
-        &:first-child { margin-top: 15px; }
-        &:last-child { margin-bottom: 15px; }
-      }
-  
-      &.art-active {
-        pointer-events: all;
-        max-height: 500px;
-  
-        .menu-item {
-          opacity: 1;
+          font-size: 11px;
         }
       }
     }
   }
-
 `;
