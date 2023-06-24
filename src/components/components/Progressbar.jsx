d439a2611efca23e6befbd01ae3c197d51275f60
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-24 23:46:27 +0300

    add styled components

--- src/components/components/Progressbar.jsx
+++ src/components/components/Progressbar.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
 import { ProgressbarInstance } from '../../utils/utils';
+import { styled } from 'styled-components';
 
 export const Progressbar = (props) => {
   const { type, level, index } = props;
@@ -9,8 +10,41 @@ export const Progressbar = (props) => {
   useEffect(() => {
     const progress = ProgressbarInstance(props);
     progress?.animate(level / 100);
-    return () => { progress?.destroy(); };
+    return () => {
+      progress?.destroy();
+    };
   }, []);
 
-  return <div id={`${type}prog${index}`} className={`art-${type}-progress`} />;
-}
+  return <Container id={`${type}prog${index}`} className={`art-${type}-progress`} />;
+};
+
+const Container = styled.div`
+  .progressbar-text {
+    position: relative;
+    left: 45% !important;
+    font-size: 11px;
+    color: var(--c-font-1) !important;
+
+    &:after {
+      content: '%';
+      position: absolute;
+      right: -7px;
+    }
+  }
+
+  &.art-circle-progress {
+    width: 65%;
+    margin: 0 auto 15px;
+  }
+
+  &.art-line-progress {
+    width: 100%;
+
+    .progressbar-text {
+      top: -6px !important;
+      width: 100%;
+      text-align: right;
+      color: var(--c-font-3) !important;
+    }
+  }
+`;
