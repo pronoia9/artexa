9ac002174fd79a22a011edb768f35ff00da4ef5c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-25 01:38:22 +0300

    move soft skills styles to styled components

--- src/components/layout/Sidebar/SoftSkills.jsx
+++ src/components/layout/Sidebar/SoftSkills.jsx
@@ -1,12 +1,43 @@
+import { styled } from 'styled-components';
+
 import { dataStore } from '../../../store/dataStore';
+import { rem } from '../../../utils';
 
 export const SoftSkills = () => {
   const data = dataStore((state) => state.sidebar.softSkills);
   return (
-    <ul className='art-knowledge-list p-15-0'>
+    <SkillsList className='art-knowledge-list p-15-0'>
       {data.map((skills, index) => (
-        <li key={`sidebar-softskills-${index}`}>{skills}</li>
+        <ListItem key={`sidebar-softskills-${index}`}>
+          <i className='fas fa-check' />
+          {skills}
+        </ListItem>
       ))}
-    </ul>
+    </SkillsList>
   );
 };
+
+const SkillsList = styled.ul`
+  padding-left: 0;
+
+  i {
+    margin-right: 10px;
+    color: var(--c-accent-1);
+    font-size: 9px;
+    font-weight: 900;
+  }
+`;
+
+const ListItem = styled.li`
+  margin-bottom: 5px;
+  color: var(--c-font-2);
+  list-style-type: none;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  &:hover i {
+    color: var(--c-accent-3);
+  }
+`;
