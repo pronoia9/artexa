1c8c4764d1c31e16084e73c8e49f78003dcac3e7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-25 01:29:42 +0300

    add progressbar to index

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,9 @@
 // Framer Motion
 // Utils
-import { getSystemTheme, getTheme, isDarkTheme, systemThemeChangeHandler, lowerCase, rem } from './utils';
+import { getSystemTheme, getTheme, isDarkTheme, systemThemeChangeHandler, lowerCase, rem, ProgressbarInstance } from './utils';
 
 export {
   // Framer Motion
   // Utils
-  getSystemTheme, getTheme, isDarkTheme, systemThemeChangeHandler, lowerCase, rem
+  getSystemTheme, getTheme, isDarkTheme, systemThemeChangeHandler, lowerCase, rem, ProgressbarInstance
 }
