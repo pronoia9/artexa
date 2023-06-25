e34355f8e57ff36d3988508c2c5983b23f1eecf5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-25 17:53:18 +0300

    fix navbar size

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -24,15 +24,15 @@ export const Navbar = () => {
 const Container = styled.div`
   position: absolute;
   top: 0;
-  right: ${rem(-230)};
+  right: ${rem(-225)};
   bottom: 0;
-  width: ${rem(330)};
+  width: ${rem(300)};
   height: calc(100vh - ${rem(30)});
   background: var(--c-bg-menu-1);
   box-shadow: 0 ${rem(3)} ${rem(8)} 0 var(--c-box-shadow);
   z-index: 99;
   isolation: isolate;
-  transform: ${({ $navbarOpen }) => $navbarOpen && `translateX(${rem(-230)})`};
+  transform: ${({ $navbarOpen }) => $navbarOpen && `translateX(${rem(-225)})`};
   transition: 0.55s ease-in-out;
 
   a {
