7a662ab336940f1a8c789b8e9f94ced761dd9d9c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-26 01:04:02 +0300

    fix gradient heading

--- src/styles/TextStyles.js
+++ src/styles/TextStyles.js
@@ -1,33 +1,36 @@
 import { styled } from 'styled-components';
 
-export const GradientHeading = styled.h1`
-  padding: 1px 0;
-  display: inline-block;
-  color: transparent;
-  background-image: var(--c-gradient-9);
-  background-size: 100%;
-  -webkit-background-clip: text;
-  -moz-background-clip: text;
-  -ms-background-clip: text;
-  -o-background-clip: text;
-  background-clip: text;
-  transition: transform 0.5s;
+import { rem } from '../utils';
+
+export const GradientHeading = styled.div`
+  transition: transform 0.5s ease-in-out;
+  padding: 1rem 0;
+
+  h1 {
+    color: transparent;
+    background-image: var(--c-gradient-9);
+    background-size: 20%;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -ms-background-clip: text;
+    background-clip: text;
+    line-height: 3rem;
+  }
 
   &:hover {
-    /* text-shadow: 0.1px 0.1px 10px var(--c-accent-2); */
+    text-shadow: ${rem(0.1)} ${rem(0.1)} ${rem(2)} var(--c-accent-2);
     transform: scale(1.01);
-    transition: transform 0.5s;
   }
 
-  @media (max-width: 1600px) {
-    font-size: 42px;
+  @media (max-width: ${rem(1600)}) {
+    font-size: ${rem(62)};
   }
 
-  @media (max-width: 1400px) {
-    font-size: 36px;
+  @media (max-width: ${rem(1400)}) {
+    font-size: ${rem(56)};
   }
 
-  @media (max-width: 700px) {
-    font-size: 46px;
+  @media (max-width: ${rem(700)}) {
+    font-size: ${rem(46)};
   }
 `;
