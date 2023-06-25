705c6e810e93e09eb893851c1223e2276b899a8b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-26 00:12:34 +0300

    export wave img

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import { avatar } from './sidebar';
 import { footer1, footer2, footer3, footer4 } from './footer';
 
 export {
-  bgBlue, bgGreen, bgPastel, bgPink, bgPurple, bgRed, bgYellow,
+  bgBlue, bgGreen, bgPastel, bgPink, bgPurple, bgRed, bgYellow, wave,
   avatar,
   footer1, footer2, footer3, footer4,
 }
