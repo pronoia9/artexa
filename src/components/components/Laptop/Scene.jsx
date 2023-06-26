7bf2143c56a38dfe61c4dfab8b9a1ce922bebd2c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-26 18:42:27 +0300

    add gradient bg + animation

--- src/components/components/Laptop/Scene.jsx
+++ src/components/components/Laptop/Scene.jsx
@@ -6,13 +6,14 @@ import { a as three } from '@react-spring/three';
 import { a as web } from '@react-spring/web';
 
 import { Laptop } from '.';
+import { keyframes, styled } from 'styled-components';
 
 export const Scene = () => {
   const [open, setOpen] = useState(false);
   const props = useSpring({ open: Number(open) });
 
   return (
-    <web.main style={{ background: props.open.to([0, 1], ['#f0f0f0', '#d25578']) }}>
+    <Container>
       <Canvas dpr={[1, 2]} camera={{ position: [0, 10, -30], fov: 35 }}>
         <three.pointLight position={[10, 10, 10]} intensity={1.5} color={props.open.to([0, 1], ['#f0f0f0', '#d25578'])} />
         <Suspense fallback={null}>
@@ -24,6 +25,22 @@ export const Scene = () => {
         <ContactShadows position={[0, -4.5, 0]} opacity={0.4} scale={20} blur={1.75} far={4.5} />
         <OrbitControls />
       </Canvas>
-    </web.main>
+    </Container>
   );
 };
+
+const bgAnimation = keyframes`
+  0% {  background-position: left; }
+  100% {  background-position: right; }
+`;
+
+const Container = styled(web.main)`
+  width: 100vw;
+  min-width: 100vw;
+  height: 10vh;
+  min-height: 100vh;
+  background: var(--c-bg);
+  background-image: linear-gradient(45deg, var(--c-accent-1), var(--c-accent-2), var(--c-accent-3), var(--c-accent-4), var(--c-accent-5));
+  background-size: 400%;
+  animation: ${bgAnimation} 20s infinite alternate;
+`;
