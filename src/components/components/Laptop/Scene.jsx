479d45d2af3b9b2c67dd0b047bc89650dc3776ba
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-26 22:32:40 +0300

    separate scene contents as a component

--- src/components/components/Laptop/Scene.jsx
+++ src/components/components/Laptop/Scene.jsx
@@ -10,6 +10,16 @@ import { Laptop } from '.';
 import { dataStore } from '../../../store/dataStore';
 
 export const Scene = () => {
+  return (
+    <Container>
+      <Canvas dpr={[1, 2]} camera={{ position: [22.5, 13, -37], fov: 35 }} >
+        <SceneContents />
+      </Canvas>
+    </Container>
+  );
+};
+
+const SceneContents = () => {
   const { laptopOpen, toggleLaptopOpen } = dataStore((state) => ({
     laptopOpen: state.laptopOpen,
     toggleLaptopOpen: state.toggleLaptopOpen,
@@ -17,19 +27,17 @@ export const Scene = () => {
   const props = useSpring({ open: Number(laptopOpen) });
 
   return (
-    <Container>
-      <Canvas dpr={[1, 2]} camera={{ position: [0, 10, -30], fov: 35 }}>
-        <three.pointLight position={[10, 10, 10]} intensity={1.5} color={props.open.to([0, 1], ['#f0f0f0', '#d25578'])} />
-        <Suspense fallback={null}>
-          <group rotation={[0, Math.PI, 0]} onClick={(e) => (e.stopPropagation(), toggleLaptopOpen())}>
-            <Laptop hinge={props.open.to([0, 1], [-3.109, -4.7])} scale={6} />
-          </group>
-          <Environment preset='city' />
-        </Suspense>
-        <ContactShadows position={[0, -4.5, 0]} opacity={0.4} scale={20} blur={1.75} far={4.5} />
-        <OrbitControls />
-      </Canvas>
-    </Container>
+    <>
+      <three.pointLight position={[10, 10, 10]} intensity={1.5} color={props.open.to([0, 1], ['#f0f0f0', '#d25578'])} />
+      <Suspense fallback={null}>
+        <group rotation={[0, Math.PI, 0]} onClick={(e) => (e.stopPropagation(), toggleLaptopOpen())}>
+          <Laptop hinge={props.open.to([0, 1], [-3.109, -4.7])} scale={6} position={[0, -4, 0]} />
+        </group>
+        <Environment preset='city' />
+      </Suspense>
+      <ContactShadows position={[0, -4.5, 0]} opacity={0.4} scale={20} blur={1.75} far={4.5} />
+      <OrbitControls />
+    </>
   );
 };
 
