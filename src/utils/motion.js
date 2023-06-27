682c8ff00ccb9db4cc237292d9a1b5afe6de9f72
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-27 17:57:07 +0300

    add delay to app container

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -7,5 +7,5 @@ export const sceneMotion = () => ({
 export const appContainerMotion = () => ({
   initial: { opacity: 0 },
   animate: { opacity: 1 },
-  transition: { duration: 0.5, ease: 'easeInOut' },
+  transition: { duration: 0.5, delay: 0.1, ease: 'easeInOut' },
 });
