bdae7ac3e25bc8d8a9fd14299327530e04e7a702
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-27 17:05:35 +0300

    move scene container

--- src/App.jsx
+++ src/App.jsx
@@ -120,15 +120,3 @@ const Container = styled.div`
     width: 100%;
   }
 `;
-
-const SceneContainer = styled(motion.div)`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  min-width: 100vw;
-  height: 100%;
-  min-height: 100vh;
-`;
