d0e1c7394e057b36df5fc4a8b365acfcc44bca61
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-28 18:38:35 +0300

    finish init preloader

--- src/components/components/Preloader.jsx
+++ src/components/components/Preloader.jsx
@@ -1,3 +1,39 @@
+import { motion } from "framer-motion";
+import { styled } from "styled-components";
+import { Progressbar } from "./Progressbar";
+
 export const Preloader = () => {
-  return <div>Preloader</div>;
+  return (
+    <Container id='art-preloader' className='art-preloader'>
+      <div className='art-preloader-content'>
+        <h1>Welcome</h1>
+        <Progressbar type='preloader' level={100} />
+      </div>
+    </Container>
+  );
 };
+
+const Container = styled(motion.div)`
+  margin: 15px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: calc(100vw - 30px);
+  height: calc(100vh - 30px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--c-font-4);
+  z-index: 999999;
+
+  .art-preloader-content {
+    margin-bottom: 15px;
+    text-align: center;
+  }
+
+  @media (max-width: 920px) {
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+`;
