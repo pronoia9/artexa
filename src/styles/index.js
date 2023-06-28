d58083e554e2b16396f316efb15e815024538725
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-28 16:46:09 +0300

    add more button styles

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,6 @@
 import GlobalStyles from './GlobalStyles';
 import { themes, colors } from './Themes';
-import { GradientButton } from './ButtonStyles';
+import { Button, ButtonMedium, ButtonSmall, GradientButton } from './ButtonStyles';
 import { GradientHeading } from './TextStyles';
 
-export { GlobalStyles, themes, colors, GradientButton, GradientHeading };
+export { GlobalStyles, themes, colors, Button, ButtonMedium, ButtonSmall, GradientButton, GradientHeading };
