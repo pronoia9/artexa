4a2f3fe54de0c14f9c6173d1253132034b8290eb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-29 00:44:04 +0300

    add init tooltip

--- src/components/layout/Sidebar/HardSkills.jsx
+++ src/components/layout/Sidebar/HardSkills.jsx
@@ -1,22 +1,26 @@
-import { styled } from "styled-components";
+import { useState } from 'react';
+import { css, styled } from 'styled-components';
 
-import { Progressbar } from "../..";
-import { dataStore } from "../../../store/dataStore";
-import { rem } from "../../../utils";
+import { Progressbar } from '../..';
+import { dataStore } from '../../../store/dataStore';
+import { Tooltip } from '../../../styles/TextStyles';
+import { rem } from '../../../utils';
 
 export const HardSkills = () => {
+  const [hover, setHover] = useState(false);
   const data = dataStore((state) => state.sidebar.skills.hard);
+
   return (
-    <div className='art-hard-skills p-30-15'>
+    <SkillsContainer className='art-hard-skills p-30-15'>
       {data.map((skill, index) => (
         <HardSkillsItem key={`sidebar-hardskill-item-${index}`} {...skill} index={index} />
       ))}
-    </div>
+    </SkillsContainer>
   );
 };
 
 export const HardSkillsItem = (props) => (
-  <ItemContainer className='art-hard-skills-item'>
+  <ItemContainer className='art-hard-skills-item' $description={props.description}>
     <div className='art-skill-heading'>
       <span>{props.title}</span>
     </div>
@@ -24,8 +28,26 @@ export const HardSkillsItem = (props) => (
   </ItemContainer>
 );
 
-const ItemContainer = styled.div`
+const SkillsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${rem(15)};
+`;
+
+const ItemContainer = styled(Tooltip)`
   position: relative;
-  margin-bottom: ${rem(15)};
   color: var(--c-font-2);
+
+  &::after {
+    max-width: 90%;
+    top: -50%;
+    right: 5%;
+    transition-delay: 1s;
+  }
+
+  ${({ $description }) => $description && css`
+    &::after {
+      content: $description;
+    }
+  `}
 `;
