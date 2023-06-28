5ca20346d58b29d83816a8f764f8d7c919975cfb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-28 16:39:58 +0300

    move hero title styles to hero title

--- src/styles/TextStyles.js
+++ src/styles/TextStyles.js
@@ -6,31 +6,14 @@ export const GradientHeading = styled.div`
   transition: transform 0.5s ease-in-out;
   padding: 1rem 0;
 
-  h1 {
+  * {
     color: transparent;
-    background-image: var(--c-gradient-9);
+    background-image: var(--c-gradient-accent-1);
     background-size: 20%;
     -webkit-background-clip: text;
     -moz-background-clip: text;
     -ms-background-clip: text;
     background-clip: text;
     line-height: 3rem;
-  }
-
-  &:hover {
-    text-shadow: ${rem(0.1)} ${rem(0.1)} ${rem(2)} var(--c-accent-2);
-    transform: scale(1.01);
-  }
-
-  @media (max-width: ${rem(1600)}) {
-    font-size: ${rem(62)};
-  }
-
-  @media (max-width: ${rem(1400)}) {
-    font-size: ${rem(56)};
-  }
-
-  @media (max-width: ${rem(700)}) {
-    font-size: ${rem(46)};
   }
 `;
