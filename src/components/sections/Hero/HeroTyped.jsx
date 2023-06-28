13b09a1c8da51c6e81be844364b7f4d3562cf148
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-28 17:22:48 +0300

    update code styles + todo: add typed

--- src/components/sections/Hero/HeroTyped.jsx
+++ src/components/sections/Hero/HeroTyped.jsx
@@ -1,50 +1,53 @@
+import { useRef } from 'react';
 import { styled } from 'styled-components';
 
 import { dataStore } from '../../../store/dataStore';
 
 export const HeroTyped = () => {
   const data = dataStore((state) => state.hero.typed);
+  const typedRef = useRef();
 
   return (
     <Container className='art-lg-text art-code mb-25'>
-      &lt;<i>code</i>&gt;{' '}
-      {/* <Typed
-          loop
-          typeSpeed={100}
-          backSpeed={25}
-          strings={props.typed}
-          smartBackspace
-          shuffle={false}
-          backDelay={1}
-          fadeOut={false}
-          fadeOutDelay={100}
-          loopCount={0}
-          showCursor
-          cursorChar='|'
-        />{' '} */}
-      &lt;/<i>code</i>&gt;
+      <code>
+        &lt;<i>code</i>&gt; <span ref={typedRef}>asfafag</span> &lt;/<i>code</i>&gt;
+      </code>
     </Container>
   );
 };
+{
+  /* <Typed
+    loop
+    typeSpeed={100}
+    backSpeed={25}
+    strings={props.typed}
+    smartBackspace
+    shuffle={false}
+    backDelay={1}
+    fadeOut={false}
+    fadeOutDelay={100}
+    loopCount={0}
+    showCursor
+    cursorChar='|'
+  />{' '} */
+}
 
 const Container = styled.div`
-  font-family: var(--f-code);
-  color: var(--c-accent-4);
-  text-shadow: 1px 1px 5px var(--c-accent-4);
+  code {
+    font-family: var(--f-code);
+    color: #e5c07b;
+    text-shadow: 1px 1px 5px var(--c-accent-4);
+    filter: ${({ theme }) => theme.typedFilter && 'saturate(50) brightness(100)'};
 
-  i {
-    font-style: normal;
-    color: var(--c-accent-3);
-    text-shadow: none;
-  }
+    i {
+      color: #ef596e;
+      font-style: normal;
+      text-shadow: none;
+    }
 
-  span {
-    color: var(--c-font-2);
-    text-shadow: none;
-
-    &:hover {
-      font-style: italic;
-      /* color: var(--c-accent-4); */
+    span {
+      color: var(--c-font-2);
+      text-shadow: none;
     }
   }
 
