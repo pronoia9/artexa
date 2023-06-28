2d567e5b0fbd2271a51be140d43581ce7d3975cf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-29 00:27:03 +0300

    init tooltip styled component

--- src/styles/TextStyles.js
+++ src/styles/TextStyles.js
@@ -17,3 +17,48 @@ export const GradientHeading = styled.div`
     line-height: 3rem;
   }
 `;
+
+export const Tooltip = styled.div`
+  position: relative;
+
+  &:after {
+    content: 'Tooltip Content';
+    position: absolute;
+    display: block;
+    font-size: ${rem(10)};
+    font-weight: 200;
+    text-align: left;
+    background: var(--c-bg);
+    box-shadow: 0 ${rem(1)} ${rem(4)} 0 var(--c-box-shadow);
+    opacity: 0;
+    pointer-events: none;
+    transform: translateX(${rem(20)});
+    transition: 0.4s ease-in-out;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    background: var(--c-bg);
+    pointer-events: none;
+    opacity: 0;
+    z-index: 99;
+    transform: translateX(${rem(20)}) rotate(45deg);
+    transition: 0.4s ease-in-out;
+    animation: none !important;
+  }
+
+  &:hover {
+    transform: scale(1);
+
+    &:after {
+      opacity: 1;
+      transform: translateX(0);
+    }
+
+    &:before {
+      opacity: 1;
+      transform: translateX(0) rotate(45deg);
+    }
+  }
+`;
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,6 @@
 import GlobalStyles from './GlobalStyles';
 import { themes, colors } from './Themes';
 import { Button, ButtonMedium, ButtonSmall, GradientButton } from './ButtonStyles';
-import { GradientHeading } from './TextStyles';
+import { GradientHeading, Tooltip } from './TextStyles';
 
-export { GlobalStyles, themes, colors, Button, ButtonMedium, ButtonSmall, GradientButton, GradientHeading };
+export { GlobalStyles, themes, colors, Button, ButtonMedium, ButtonSmall, GradientButton, GradientHeading, Tooltip };
