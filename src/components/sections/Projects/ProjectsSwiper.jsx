53c1bbb5dd825df58ea2dd825ef237d5f98d3aee
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-30 22:28:44 +0300

    simplify structure

--- src/components/sections/Projects/ProjectsSwiper.jsx
+++ src/components/sections/Projects/ProjectsSwiper.jsx
@@ -9,36 +9,32 @@ const ProjectsSwiper = () => {
   SwiperCore.use([Autoplay]);
 
   return (
-    <div className='row p-30-0'>
-      {/* title */}
+    <>
       <SectionTitle title='Projects' />
 
-      {/* project items  */}
-      <div className='col-lg-12'>
-        <Swiper
-          modules={[Navigation, Pagination, A11y]}
-          spaceBetween={30}
-          slidesPerView={3}
-          speed={900}
-          autoplay={{ delay: 3000 }}
-          breakpoints={{
-            320: { slidesPerView: 1, spaceBetween: 10 },
-            640: { slidesPerView: 2, spaceBetween: 10 },
-            1200: { slidesPerView: 3, spaceBetween: 30 },
-          }}
-          navigation={{ nextEl: '.art-project-swiper-next', prevEl: '.art-project-swiper-prev' }}
-          pagination={{ el: '.project-swiper-pagination', type: 'bullets', clickable: true }}
-        >
-          {projects.map((project, index) => (
-            <SwiperSlide key={`projects-swiper-slide-${index}`} className='swiper-slide art-grid art-grid-3-col art-gallery'>
-              <ProjectsCard key={`projects-projects-card-${index}`} swiper={true} index={index} {...project} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <Swiper
+        modules={[Navigation, Pagination, A11y]}
+        spaceBetween={30}
+        slidesPerView={3}
+        speed={900}
+        autoplay={{ delay: 3000 }}
+        breakpoints={{
+          320: { slidesPerView: 1, spaceBetween: 10 },
+          640: { slidesPerView: 2, spaceBetween: 10 },
+          1200: { slidesPerView: 3, spaceBetween: 30 },
+        }}
+        navigation={{ nextEl: '.art-project-swiper-next', prevEl: '.art-project-swiper-prev' }}
+        pagination={{ el: '.project-swiper-pagination', type: 'bullets', clickable: true }}
+      >
+        {projects.map((project, index) => (
+          <SwiperSlide key={`projects-swiper-slide-${index}`} className='swiper-slide art-grid art-grid-3-col art-gallery'>
+            <ProjectsCard key={`projects-projects-card-${index}`} swiper={true} index={index} {...project} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <SwiperNavigation section='project' />
-    </div>
+    </>
   );
 };
 export default SectionWrapper(ProjectsSwiper, 'projects-swiper');
