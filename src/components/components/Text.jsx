f4374b6141c62b23db550b568a91487e8e6d7f42
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-30 21:58:07 +0300

    add styles + optimize text component

--- src/components/components/Text.jsx
+++ src/components/components/Text.jsx
@@ -1,22 +1,55 @@
-export const Title = ({ title }) => (
-  <div className='art-section-title'>
-    <div className='art-title-frame'>
-      <h4>{title}</h4>
-    </div>
+import { styled } from 'styled-components';
+
+export const Title = (props) => (
+  <TitleContainer className='art-section-title'>
+    <TitleFrame className='art-title-frame'>
+      <h2>{props.title}</h2>
+    </TitleFrame>
+    {props.children}
+  </TitleContainer>
+);
+
+export const SectionTitle = ({ column = 1, title, rightTitle, rightClass }) => (
+  <div className={`col-lg-${12 / column}`}>
+    <Title title={title}>
+      {rightTitle && (
+        <TitleRightFrame className='art-right-frame'>
+          <div className={rightClass}>{rightTitle}</div>
+        </TitleRightFrame>
+      )}
+    </Title>
   </div>
 );
 
-export const SectionTitle = ({ column, title, rightTitle, rightClass }) => (
-  <div className={'col-lg-' + (12 / column || 12)}>
-    <div className='art-section-title'>
-      <div className='art-title-frame'>
-        <h4>{title}</h4>
-      </div>
-      {rightTitle && (
-        <div className='art-right-frame'>
-          <div className={rightClass}>{rightTitle}</div>
-        </div>
-      )}
-    </div>
-  </div>
-);
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+
+  @media (max-width: 500px) {
+    flex-direction: column;
+  }
+`;
+
+const TitleFrame = styled.div`
+  align-self: center;
+  position: relative;
+  margin-bottom: 30px;
+`;
+
+const TitleRightFrame = styled.div`
+  padding-bottom: 30px;
+
+  @media (max-width: 768px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  @media (max-width: 500px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
