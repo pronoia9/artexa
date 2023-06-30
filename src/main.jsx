3770abe80c1fced6cbd46b8df648105d186b8de9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-30 21:48:01 +0300

    import swiper styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,28 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
 import 'swiper/css';
+import 'swiper/css/a11y';
+import 'swiper/css/autoplay';
+// import 'swiper/css/controller';
+// import 'swiper/css/effect-cards';
+// import 'swiper/css/effect-coverflow';
+// import 'swiper/css/effect-creative';
+// import 'swiper/css/effect-cube';
+// import 'swiper/css/effect-fade';
+// import 'swiper/css/effect-flip';
+// import 'swiper/css/free-mode';
+// import 'swiper/css/grid';
+import 'swiper/css/history';
+import 'swiper/css/keyboard';
+import 'swiper/css/manipulation';
+import 'swiper/css/mousewheel';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+// import 'swiper/css/parallax';
 import 'swiper/css/scrollbar';
+// import 'swiper/css/thumbs';
+// import 'swiper/css/virtual';
+// import 'swiper/css/zoom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
