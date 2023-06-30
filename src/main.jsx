6177297bf7b5436346dd484b2f1d2818713ebdb0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-30 21:44:06 +0300

    import more swiper styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
