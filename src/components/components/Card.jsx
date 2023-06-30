9b3d08ba85e6d72531f160eb8f409236431a1701
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-30 22:59:32 +0300

    separate card as a component

--- src/components/components/Card.jsx
+++ src/components/components/Card.jsx
@@ -1,24 +1,32 @@
 import { Link } from "react-router-dom";
+import { styled } from "styled-components";
 
 export const Card = ({ classes = '', title = 'Title', subtitle, link = '/', image = '/src/assets/hero/bg-blue.jpg', ...props }) => {
   return (
-    <div class={`art-a art-blog-card${classes ? ` ${classes}` : ''}`}>
-      <a data-fancybox='projects' href='/src/assets/hero/bg-blue.jpg' className={'art-a art-portfolio-item-frame art-square'}>
+    <Container className={`art-a art-blog-card${classes ? ` ${classes}` : ''} acl`}>
+      <ImageLink data-fancybox='projects' href='/src/assets/hero/bg-blue.jpg' className={'art-a art-portfolio-item-frame art-square'}>
         <img src={image} alt='item' />
         <span className='art-item-hover'>
           <i className='fas fa-expand' />
         </span>
-      </a>
-      <div class='art-post-description'>
-        {/* <a href='#.'> */}<h5 class='mb-15'>{title}</h5>{/* </a> */}
-        {subtitle && <div class='mb-15'>{subtitle}</div>}
+      </ImageLink>
+      <Overlay className='art-post-description'>
+        {/* <a href='#.'> */}<Title className='mb-15'>{title}</Title>{/* </a> */}
+        {subtitle && <div className='mb-15'>{subtitle}</div>}
 
         {props.children}
 
-        <Link to={link} className='art-link art-color-link art-w-chevron'>
-          Read More
-        </Link>
-      </div>
-    </div>
+        <ProjectLink to={link} className='art-link art-color-link art-w-chevron'>Read More</ProjectLink>
+      </Overlay>
+    </Container>
   );
 };
+const Container = styled.div``;
+
+const ImageLink = styled.a``;
+
+const Overlay = styled.div``;
+
+const Title = styled.h5``;
+
+const ProjectLink = styled(Link)``;
--- src/components/sections/Projects/ProjectsCard.jsx
+++ src/components/sections/Projects/ProjectsCard.jsx
@@ -1,28 +1,35 @@
+import { Link } from 'react-router-dom';
+
+import { styled } from 'styled-components';
+import { Card } from '../..';
+
 export const ProjectsCard = ({ index, swiper, id, title, subtitle, description, link, links, image, images, year, date, categories, tags }) => {
   return (
-    <div className={`art-grid-item${swiper ? ' art-project-swiper-card' : ''}`}>
-      <a data-fancybox='projects' href='/src/assets/hero/bg-blue.jpg' className={'art-a art-portfolio-item-frame art-square'}>
-        <img src='/src/assets/hero/bg-blue.jpg' alt='item' />
-        <span className='art-item-hover'>
-          <i className='fas fa-expand' />
-        </span>
-      </a>
-
-      <div className='art-item-description'>
-        <h5 className='mb-15'>{title}</h5>
-        {!swiper && <div className='mb-15'>{subtitle}</div>}
-        <div className='mb-15'>
+    <>
+      <Card title={title} link={`/projects/${id}`}>
+        <TagsContainer className='mb-15'>
           {[swiper ? categories : tags].flat().map((tag) => (
             <p key={`project-${title}-tags-${tag}`} className='art-tag'>
               {tag}
             </p>
           ))}
-        </div>
-
-        <a href={`/projects/${id}`} className='art-link art-color-link art-w-chevron' target='_blank' rel='noreferrer'>
-          Read More
-        </a>
-      </div>
-    </div>
+        </TagsContainer>
+      </Card>
+    </>
   );
 };
+
+const TagsContainer = styled.div`
+  min-height: 58px;  /* 2 lines */
+
+  p {
+    display: inline-block;
+    color: var(--c-font-1);
+    background: var(--c-bg);
+    padding: 3px 12px;
+    border-radius: 15px;
+    box-shadow: inset 0 3px 8px 0 var(--c-box-shadow);
+    font-size: 10px;
+    margin: 0 5px 5px 0;
+  }
+`;
