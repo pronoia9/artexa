d978a91907529468b955ae8eb798a9d4dc78c459
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-01 18:07:40 +0300

    add some states (ongoing)

--- src/components/sections/Projects/ProjectsGrid.jsx
+++ src/components/sections/Projects/ProjectsGrid.jsx
@@ -1,23 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
 import { styled } from 'styled-components';
 import { motion } from 'framer-motion';
 
 import { ProjectsFilter, SectionWrapper, SectionTitle, ProjectsCard } from '../..';
 import { dataStore } from '../../../store/dataStore';
+import { lowerCase, setProjectsCount } from '../../../utils';
 
 const ProjectsGrid = () => {
-  const data = dataStore((state) => state.projects);
-  const { filters, projects } = data;
+  const data = dataStore((state) => state.projects.projects);
+  const [filterKey, setFilterKey] = useState(''),
+    [projects, setProjects] = useState(data),
+    [cols, setCols] = useState(2),
+    [rows, setRows] = useState(3),
+    [count, setCount] = useState(2 * 3);
+  const topRef = useRef();
+
+  // Checks whether or not all the projects are shown with the filter applied
+  const showingAllProjects = () => !(projects.slice(0, count).length < projects.length);
+
+  // Handle filtering projects when filter key changes
+  useEffect(() => {
+    setProjects(
+      [!filterKey
+        ? data
+        : data.filter(
+            (p) => lowerCase(p.categories.join('')).includes(filterKey) || lowerCase(p.tags.join('')).includes(filterKey)),
+      ].flat()
+    );
+  }, [filterKey]);
+
+  // Update projects count whenever cols or rows changes
+  useEffect(() => setCount(rows * cols), [rows, cols]);
 
   return (
     <Container className='row p-30-0'>
       <SectionTitle title='Projects'>
         <ProjectsFilter />
       </SectionTitle>
-      <div className={'art-grid art-grid-2-col art-gallery'}>
-        {data.projects.map((project, index) => (
-          <ProjectsCard key={`projects-grid-item-${index}`} index={index} classes='art-grid-item' {...project} />
+
+      <Grid className={`art-grid art-grid-${cols}-col art-gallery`}>
+        {projects.map((project, index) => (
+          <ProjectsCard key={`projects-grid-item-${index}`} index={index} hide={true} classes='art-grid-item' {...project} />
         ))}
-      </div>
+      </Grid>
     </Container>
   );
 };
@@ -26,3 +51,5 @@ export default SectionWrapper(ProjectsGrid, 'projects-grid');
 const Container = styled(motion.div)`
   padding-top: 30px;
 `;
+
+const Grid = styled.div``;
