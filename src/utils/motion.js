6f0de6bb452198111e202e16d9f336eba343b58d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-01 22:27:01 +0300

    adjust projects grid/card motion

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -245,7 +245,8 @@ export const countersMotion = {
 export const projectsMotion = {
   container: { ...motionStates(), variants: staggerContainer(0.5, 4) },
   swiper: { variants: staggerContainerMirror(0.5) },
-  card: { variants: zoomIn({ type: 'tween', duration: 0.1, ease: 'easeIn', staggerChildren: 0.1 }) },
+  grid: { variants: staggerContainerMirror(0.05) },
+  card: { variants: zoomIn({ type: 'tween', duration: 0.01, ease: 'linear', staggerChildren: 0.1 }) },
   navigation: { variants: fadeIn({ delay: 6.25 }) },
   filters: { variants: staggerContainer(0.15) },
   filter: { variants: zoomIn({ type: 'tween', duration: 0.1, ease: 'easeIn' }) },
