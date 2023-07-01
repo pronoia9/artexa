3b5f1661517a1bdecdf70f31a808453fbd181ebf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-01 16:35:54 +0300

    add/finish init projects filter component

--- src/components/sections/Projects/ProjectsFilter.jsx
+++ src/components/sections/Projects/ProjectsFilter.jsx
@@ -1,3 +1,15 @@
-export const ProjectsFilter = () => {
-  return <div>ProjectsFilter</div>;
+import { dataStore } from '../../../store/dataStore';
+
+export const ProjectsFilter = ({ handleClick }) => {
+  const data = dataStore((state) => state.projects.filters);
+
+  return (
+    <div className='art-filter'>
+      {data.map(({ title, category }, index) => (
+        <div key={`projects-filter-${index}`} className='art-link' onClick={() => handleClick(category)}>
+          {title}
+        </div>
+      ))}
+    </div>
+  );
 };
