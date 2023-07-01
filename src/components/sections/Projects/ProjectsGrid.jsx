0082b768f7e5a16638f435391fbfe2835bc0d321
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-01 16:37:13 +0300

    add init projects grid content

--- src/components/sections/Projects/ProjectsGrid.jsx
+++ src/components/sections/Projects/ProjectsGrid.jsx
@@ -1,4 +1,25 @@
+import { styled } from 'styled-components';
+import { ProjectsFilter, SectionWrapper, SectionTitle, Card } from '../..';
+import { dataStore } from '../../../store/dataStore';
+import { motion } from 'framer-motion';
+
 const ProjectsGrid = () => {
-  return <div>ProjectsGrid</div>;
+  const data = dataStore((state) => state.projects);
+  const { filters, projects } = data;
+
+  return (
+    <Container>
+      <SectionTitle title='Projects'>
+        <ProjectsFilter />
+      </SectionTitle>
+      <div className={'art-grid art-grid-2-col art-gallery'}>
+        {data.projects.map((project, index) => <Card key={`projects-grid-item-${index}`} index={index} {...project} />)}
+      </div>
+    </Container>
+  );
 };
-export default ProjectsGrid;
+export default SectionWrapper(ProjectsGrid, 'projects-grid');
+
+const Container = styled(motion.div)`
+  padding-top: 30px;
+`;
