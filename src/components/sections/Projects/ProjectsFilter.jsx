553b6fd5c7f33facdd4e3b456b4f485bbf6630a7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-01 18:16:20 +0300

    add acc class

--- src/components/sections/Projects/ProjectsFilter.jsx
+++ src/components/sections/Projects/ProjectsFilter.jsx
@@ -4,9 +4,9 @@ export const ProjectsFilter = ({ handleClick }) => {
   const data = dataStore((state) => state.projects.filters);
 
   return (
-    <div className='art-filter'>
+    <div className='art-filter acc'>
       {data.map(({ title, category }, index) => (
-        <div key={`projects-filter-${index}`} className='art-link' onClick={() => handleClick(category)}>
+        <div key={`projects-filter-${index} acc`} className='art-link' onClick={() => handleClick(category)}>
           {title}
         </div>
       ))}
