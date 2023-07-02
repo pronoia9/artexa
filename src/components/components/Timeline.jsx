9a5faf111b1c9e799be137549eee80cd3537054c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-02 21:01:43 +0300

    upgrade timeline

--- src/components/components/Timeline.jsx
+++ src/components/components/Timeline.jsx
@@ -1,62 +1,71 @@
 import { SectionTitle } from '..';
 
-export const Timeline = ({ column, title, list }) => <TimelineColumn column={props.column} title={props.title} list={props.list} />;
+export const Timeline = ({ titles, children }) =>
+  children.map((tc, index) => <TimelineColumn column={children.length} title={titles[index]} key={`timeline-column-${index}`} data={tc} />);
 
-export const TimelineColumn = ({ column, title, list }) => (
-  // half screen panel
+export const TimelineColumn = ({ column = 1, title = '', data = [] }) => (
   <div className={'col-lg-' + 12 / column}>
-    {/* title */}
     <SectionTitle title={title} />
 
-    {/* items container */}
     <div className='art-timeline art-gallery'>
-      {list.map((item) => (
+      {data.map((item) => (
         <TimelineItem key={item.id} {...item} />
       ))}
     </div>
   </div>
 );
 
-export const TimelineItem = ({ id, title, subtitle, date, description, button, url, fancybox }) => (
-  <div className='art-timeline-item'>
-    {/* decoration, the circle lights next to the item */}
-    <div className='art-timeline-mark-light'></div>
-    <div className='art-timeline-mark'></div>
+export const TimelineItem = (props) => {
+  const { id, title, subtitle, description, dates, button, url, fancybox } = props;
 
-    {/* item */}
-    <div className='art-a art-timeline-content'>
-      {/* header */}
-      <div className='art-card-header'>
-        <div className='art-left-side'>
-          {/* title */}
-          <h5>{title}</h5>
-          {/* status / author */}
-          {subtitle && <div className='art-el-suptitle mb-15'>{subtitle}</div>}
-        </div>
-        {/* date */}
-        {date && (
-          <div className='art-right-side'>
-            <div className='art-date'>{date}</div>
+  dates[0] = new Date(dates[0]).toLocaleDateString('en-us', { year: 'numeric', month: 'short' });
+  dates[1] = dates[1]
+    ? dates[1] === 'Ongoing'
+      ? 'Ongoing'
+      : new Date(dates[1]).toLocaleDateString('en-us', { year: 'numeric', month: 'short' })
+    : null;
+
+  return (
+    <div className='art-timeline-item'>
+      {/* decoration, the circle lights next to the item */}
+      <div className='art-timeline-mark-light'></div>
+      <div className='art-timeline-mark'></div>
+
+      {/* item */}
+      <div className='art-a art-timeline-content'>
+        {/* header */}
+        <div className='art-card-header'>
+          <div className='art-left-side'>
+            {/* title */}
+            <h5>{title}</h5>
+            {/* status / author */}
+            {subtitle && <div className='art-el-suptitle mb-15'>{subtitle}</div>}
           </div>
-        )}
+          {/* date */}
+          {dates.length && (
+            <div className='art-right-side'>
+              <div className='art-date'>{`${dates[0]}${dates[1] && dates[0] !== dates[1] ? ` - ${dates[1]}` : ''}`}</div>
+            </div>
+          )}
+        </div>
+
+        {/* content */}
+        <p>{description}</p>
+        {/* <ul> {list.map((c) => ( <li key={c}>{c}</li> ))} </ul> */}
+
+        {/* link */}
+        {button &&
+          url &&
+          (fancybox ? (
+            <a data-fancybox='history' className='art-link art-color-link art-w-chevron' href={process.env.PUBLIC_URL + url}>
+              {button}
+            </a>
+          ) : (
+            <a className='art-link art-color-link art-w-chevron' href={url} target='_blank' rel='noreferrer'>
+              {button}
+            </a>
+          ))}
       </div>
-
-      {/* content */}
-      <p>{description}</p>
-      {/* <ul> {list.map((c) => ( <li key={c}>{c}</li> ))} </ul> */}
-
-      {/* link */}
-      {button &&
-        url &&
-        (fancybox ? (
-          <a data-fancybox='history' className='art-link art-color-link art-w-chevron' href={process.env.PUBLIC_URL + url}>
-            {button}
-          </a>
-        ) : (
-          <a className='art-link art-color-link art-w-chevron' href={url} target='_blank' rel='noreferrer'>
-            {button}
-          </a>
-        ))}
     </div>
-  </div>
-);
+  );
+};
