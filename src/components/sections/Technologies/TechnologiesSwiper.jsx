8c93d191634fe8ff7bf58e9418adc21a91009c4b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-02 23:22:43 +0300

    add init tech swiper

--- src/components/sections/Technologies/TechnologiesSwiper.jsx
+++ src/components/sections/Technologies/TechnologiesSwiper.jsx
@@ -1,7 +1,19 @@
+import { SwiperSlide } from 'swiper/react';
 
-import { SectionWrapper } from '../..';
+import { Swiper, Card } from '../..';
+import { dataStore } from '../../../store/dataStore';
+import { technologiesMotion } from '../../../utils';
 
-const TechnologiesSwiper = () => {
-  return <div>TechnologiesSwiper</div>;
+export const TechnologiesSwiper = () => {
+  const data = dataStore((state) => state.technologies.list);
+
+  return (
+    <Swiper section='technologies' navProps={technologiesMotion.navigation} autoplay={true} {...technologiesMotion.swiper}>
+      {data.map((tech, index) => (
+        <SwiperSlide key={`technologies-swiper-slide-${index}`} className='swiper-slide art-grid art-grid-3-col art-gallery'>
+          <Card key={`technologies-swiper-card-${index}`} swiper={true} index={index} {...tech} motion={technologiesMotion.card} subtitle={null} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
-export default SectionWrapper(TechnologiesSwiper, 'technologies-swiper');
