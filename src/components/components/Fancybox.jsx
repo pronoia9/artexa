ea1d252f7b0a4deca748a7eeede8f11440bded52
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-02 18:19:54 +0300

    revert to init fancybox component

--- src/components/components/Fancybox.jsx
+++ src/components/components/Fancybox.jsx
@@ -1,17 +1,17 @@
-import { useRef, useEffect } from "react";
-import { Fancybox as NativeFancybox } from "@fancyapps/ui";
-import { styled } from "styled-components";
-import { motion } from "framer-motion";
+import { useRef, useEffect } from 'react';
+import { Fancybox as NativeFancybox } from '@fancyapps/ui';
+import { styled } from 'styled-components';
+import { motion } from 'framer-motion';
 
-import { rem } from "../../utils";
+import { rem } from '../../utils';
 
-export const Fancybox = (props) => {
+export const Fancybox = ({ styles, ...props }) => {
   const containerRef = useRef(null);
 
   useEffect(() => {
     const container = containerRef.current;
 
-    const delegate = props.delegate || "[data-fancybox]";
+    const delegate = props.delegate || '[data-fancybox]';
     const options = props.options || {};
 
     NativeFancybox.bind(container, delegate, options);
@@ -23,52 +23,9 @@ export const Fancybox = (props) => {
   });
 
   return (
-    <Container ref={containerRef} {...props}>
+    <motion.div ref={containerRef} {...props}>
       {props.children}
       <i className='fas fa-expand' />
-    </Container>
+    </motion.div>
   );
-}
-
-const Container = styled(motion.div)`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  outline: inherit;
-  transition: 0.2s ease-in-out;
-  z-index: 0;
-
-  i {
-    width: ${rem(30)};
-    height: ${rem(30)};
-    align-self: center;
-    color: var(--c-font-2);
-    font-size: ${rem(11)};
-    font-weight: 700;
-    line-height: ${rem(30)};
-    text-align: center;
-    background: var(--c-gradient-3);
-    border-radius: 50%;
-    opacity: 0;
-    z-index: 1;
-    transition: 0.4s ease-in-out;
-  }
-
-  &:hover {
-    opacity: 1;
-    transition: 0.2s ease-in-out;
-    outline: inherit;
-
-    i {
-      opacity: 0.7;
-
-      &:hover {
-        opacity: 1;
-        transform: scale(1.07);
-      }
-    }
-  }
-`;
+};
