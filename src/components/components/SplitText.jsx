9c1f1ea894c4347d71a6caa5357a1eda298ce14d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-02 16:11:53 +0300

    update split text props for specific motion

--- src/components/components/SplitText.jsx
+++ src/components/components/SplitText.jsx
@@ -2,12 +2,12 @@ import { motion } from 'framer-motion';
 
 import { textType } from '../../utils';
 
-export const SplitText = ({ speed, children }) => {
+export const SplitText = ({ speed, children, text, char }) => {
   return (
-    <motion.span className='split-text' {...textType.text(speed)}>
-      {`${children}`.split('').map((char, index) => (
-        <motion.span key={`split-text-${children}-${index}-${char}`} {...textType.char}>
-          {char}
+    <motion.span className='split-text' {...textType.text(speed)} {...text}>
+      {`${children}`.split('').map((c, index) => (
+        <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...char}>
+          {c}
         </motion.span>
       ))}
     </motion.span>
