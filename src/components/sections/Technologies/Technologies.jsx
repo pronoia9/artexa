7a83d68b69ac32f79335a45a1c88c28cae83c30a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-02 23:09:19 +0300

    init technologies content

--- src/components/sections/Technologies/Technologies.jsx
+++ src/components/sections/Technologies/Technologies.jsx
@@ -1,14 +1,14 @@
 import { motion } from 'framer-motion';
 
-import { SectionWrapper, SectionTitle } from '../..';
+import { SectionWrapper, SectionTitle, TechnologiesGrid, TechnologiesSwiper } from '../..';
 import { technologyMotion } from '../../../utils';
 
 export const Technologies = ({ type = 'swiper', limit, ...props }) => {
   return (
-    <motion.div className='row p-30-0' {...technologyMotion?.container()} {...props}>
+    <motion.div className='row p-30-0' {...technologyMotion?.container} {...props}>
       <SectionTitle title='Techolonies' />
-      {/* {type === 'grid' && <ProjectsGrid limit={limit} />} */}
-      {/* {type === 'swiper' && <ProjectsSwiper />} */}
+      {type === 'grid' && <TechnologiesGrid limit={limit} />}
+      {type === 'swiper' && <TechnologiesSwiper />}
     </motion.div>
   );
 };
