f7b849291c7c53bcb7d09b1cba0918061a4e1083
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-02 21:18:37 +0300

    finish init history

--- src/components/sections/History/History.jsx
+++ src/components/sections/History/History.jsx
@@ -1,7 +1,19 @@
-import { SectionWrapper } from '../..';
+import { motion } from 'framer-motion';
+
+import { SectionWrapper, Timeline } from '../..';
+import { dataStore } from '../../../store/dataStore';
 
 const History = () => {
-  return <div>History</div>;
+  const data = dataStore((state) => state.history);
+
+  return (
+    <motion.div className='row p-60-0'>
+      <Timeline titles={['Certificates', 'Courses']}>
+        {data.education}
+        {data.courses}
+      </Timeline>
+    </motion.div>
+  );
 };
 
 export default SectionWrapper(History, 'history');
