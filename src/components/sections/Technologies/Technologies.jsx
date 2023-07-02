21f1d52db1d5cc3c62367046bdfda9b01c672462
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-02 23:11:55 +0300

    rename technologiesMotion

--- src/components/sections/Technologies/Technologies.jsx
+++ src/components/sections/Technologies/Technologies.jsx
@@ -1,11 +1,11 @@
 import { motion } from 'framer-motion';
 
 import { SectionWrapper, SectionTitle, TechnologiesGrid, TechnologiesSwiper } from '../..';
-import { technologyMotion } from '../../../utils';
+import { technologiesMotion } from '../../../utils';
 
 export const Technologies = ({ type = 'swiper', limit, ...props }) => {
   return (
-    <motion.div className='row p-30-0' {...technologyMotion?.container} {...props}>
+    <motion.div className='row p-30-0' {...technologiesMotion?.container} {...props}>
       <SectionTitle title='Techolonies' />
       {type === 'grid' && <TechnologiesGrid limit={limit} />}
       {type === 'swiper' && <TechnologiesSwiper />}
