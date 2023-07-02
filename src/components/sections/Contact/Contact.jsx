ebbb61e7a9cad4266a1f548ac5169fbb93cb6ca8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-03 00:04:52 +0300

    add init contact content

--- src/components/sections/Contact/Contact.jsx
+++ src/components/sections/Contact/Contact.jsx
@@ -1,6 +1,19 @@
-import { SectionWrapper } from '../..';
+import { ContactCard, ContactForm, SectionTitle, SectionWrapper } from '../..';
+import { dataStore } from '../../../store/dataStore';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const data = dataStore((state) => state.contact);
+
+  return (
+    <div className='row p-30-0'>
+      <SectionTitle title={data.title} />
+
+      {data.info.map((info, index) => (
+        <ContactCard key={`contact-card-${index}`} {...info} length={data.info.length} />
+      ))}
+
+      <ContactForm />
+    </div>
+  );
 };
 export default SectionWrapper(Contact, 'contact');
