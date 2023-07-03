bf9c3262aa4dda79b41eeeb823f84377d1cd3fd2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-03 23:16:05 +0300

    add/fix styles

--- src/components/sections/About/HobbiesCard.jsx
+++ src/components/sections/About/HobbiesCard.jsx
@@ -1,12 +1,23 @@
+import { motion } from "framer-motion";
+import { styled } from "styled-components";
+
 export const HobbiesCard = ({ title, image }) => {
   return (
-    <div className={'art-a art-hobbies-item-frame'}>
-      <a data-fancybox='hobbies' href={image} className={'art-a art-hobbies-item-frame art-horizontal'}>
+    <Frame className='art-a art-hobbies-item-frame acc'>
+      <a data-fancybox='hobbies' href={image} className='art-a art-hobbies-item-frame art-horizontal'>
         <img src={image} alt={title} />
-        <span className='art-item-hover'>
+        <span className='art-item-hover center'>
           <i className='fas fa-expand' />
         </span>
       </a>
-    </div>
+    </Frame>
   );
 };
+
+const Frame = styled(motion.div)`
+  &:hover {
+    .art-item-hover {
+      opacity: 0.8;
+    }
+  }
+`;
