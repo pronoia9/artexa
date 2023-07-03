b6b9bb94e107b07f981090878ef4ece499075960
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-03 17:55:28 +0300

    format

--- src/components/sections/Contact/ContactForm.jsx
+++ src/components/sections/Contact/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
 import { SectionTitle, ContactInput } from '../../';
