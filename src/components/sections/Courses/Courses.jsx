f479eae9495fea52825a16b0b2e9b036c21c0003
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-03 22:14:08 +0300

    add timeline

--- src/components/sections/Courses/Courses.jsx
+++ src/components/sections/Courses/Courses.jsx
@@ -1,5 +1,8 @@
-import { SectionWrapper } from '../..';
+import { SectionWrapper, Timeline } from '../..';
+import { dataStore } from '../../../store/dataStore';
 
 export default SectionWrapper(() => {
-  return <div>Courses</div>;
+  const data = dataStore((state) => state.courses);
+
+  return data.map((col, index) => <Timeline key={`courses-${index}-${col?.title}`} {...col} column={2} />);
 }, 'courses');
