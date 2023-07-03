2d1eead77a4098dd0d90d47d915e576236f77577
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-03 22:13:57 +0300

    update timeline with alternate methods of using it

--- src/components/components/Timeline/Timeline.jsx
+++ src/components/components/Timeline/Timeline.jsx
@@ -1,4 +1,11 @@
-import { TimelineColumn } from "../..";
+import { TimelineColumn } from '../..';
 
-export const Timeline = ({ titles, children }) =>
-  children.map((tc, index) => <TimelineColumn column={children.length} title={titles[index]} key={`timeline-column-${index}`} data={tc} />);
+export const Timeline = ({ titles, children, ...props }) => {
+  return children ? (
+    children.map((tl, index) => (
+      <TimelineColumn column={children.length || 2} title={titles[index] || ''} key={`timeline-column-${index}`} data={tl} />
+    ))
+  ) : (
+    <TimelineColumn {...props} /> // column={column || 2} title={title || ''} key={`timeline-column-${index}`} data={data}
+  );
+};
