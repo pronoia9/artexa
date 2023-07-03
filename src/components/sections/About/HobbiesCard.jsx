f1bfbcc0ffb8244cec4a95a6ff16fd2bf4088087
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-03 22:58:05 +0300

    add init hobbies card

--- src/components/sections/About/HobbiesCard.jsx
+++ src/components/sections/About/HobbiesCard.jsx
@@ -1,3 +1,12 @@
-export const HobbiesCard = () => {
-  return <div>HobbiesCard</div>;
+export const HobbiesCard = ({ title, image }) => {
+  return (
+    <div className={'art-a art-hobbies-item-frame'}>
+      <a data-fancybox='hobbies' href={image} className={'art-a art-hobbies-item-frame art-horizontal'}>
+        <img src={image} alt={title} />
+        <span className='art-item-hover'>
+          <i className='fas fa-expand' />
+        </span>
+      </a>
+    </div>
+  );
 };
