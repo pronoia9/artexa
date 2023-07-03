8b84f24eb211694f681b311e7c5766a1855e7850
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-03 15:37:05 +0300

    add section motions as props

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,14 +1,15 @@
 import { Contact, Counters, Hero, History, PageWrapper, Projects, Technologies } from '..';
+import { contactMotion, heroMotion, historyMotion, projectsMotion, technologiesMotion } from '../../utils';
 
 const HomePage = () => {
   return (
     <>
-      <Hero />
-      <Counters />
-      <Projects type='grid' limit={true} />
-      <History />
-      <Technologies type='swiper' />
-      <Contact />
+      <Hero motion={heroMotion.hero} />
+      <Counters motion={{}} />
+      <Projects motion={projectsMotion.container()} type='grid' limit={true} />
+      <History motion={historyMotion} />
+      <Technologies motion={technologiesMotion.container} type='swiper' />
+      <Contact motion={contactMotion} />
     </>
   );
 };
