e792b6a36e5174508b21c058753c4ceb11fda619
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-03 22:57:59 +0300

    finish init hobbies

--- src/components/sections/About/Hobbies.jsx
+++ src/components/sections/About/Hobbies.jsx
@@ -1,3 +1,28 @@
+import { dataStore } from '../../../store/dataStore';
+import { HobbiesCard, Swiper } from '../..';
+import { SwiperSlide } from 'swiper/react';
+
 export const Hobbies = () => {
-  return <div>Hobbies</div>;
+  const data = dataStore((state) => state.about.hobbies);
+
+  const swiperOptions = {
+    modules: null,
+    spaceBetween: 0,
+    slidesPerView: 5,
+    speed: 500,
+    autoplay: { delay: 2000 },
+    breakpoints: { 320: { slidesPerView: 3 }, 640: { slidesPerView: 4 }, 1200: { slidesPerView: 5 } },
+    navigation: false,
+    pagination: false,
+  };
+
+  return (
+    <Swiper section='hobbies' autoplay={true} swiperOptions={swiperOptions} navigation={false}>
+      {data.map((hobby, index) => (
+        <SwiperSlide key={`hobbies-${index}`}>
+          <HobbiesCard {...hobby} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
