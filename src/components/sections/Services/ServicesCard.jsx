5ea1cee252c3edb9078f19c2452369bc559174f2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-04 00:49:22 +0300

    add styles

--- src/components/sections/Services/ServicesCard.jsx
+++ src/components/sections/Services/ServicesCard.jsx
@@ -2,12 +2,12 @@ import { styled } from "styled-components";
 
 export const ServicesCard = ({ title, subtitle, description, link, ...props }) => {
   return (
-    <div className='col-lg-4 col-md-6'>
+    <div className='col-lg-4 col-md-6 col-sm-12 mb-30'>
       <Card className='art-a art-card art-service-icon-box'>
         <Wrapper className='art-service-ib-content'>
           <div>
             <h5 className='mb-15'>{title}</h5>
-            <div className='mb-15'>{description}</div>
+            <p>{description}</p>
           </div>
           <div className='art-buttons-frame'>
             <a className='art-link art-color-link art-w-chevron' href={link}>
@@ -21,7 +21,22 @@ export const ServicesCard = ({ title, subtitle, description, link, ...props }) =
 };
 
 const Card = styled.div`
-  height: 260px;
+  height: 100%;
+
+  h5 {
+    font-family: var(--f-primary);
+    min-height: 4ch;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  @media (max-width: 920px) {
+    h5 {
+      min-height: 1ch;
+    }
+  }
 `;
 
 const Wrapper = styled.div`
