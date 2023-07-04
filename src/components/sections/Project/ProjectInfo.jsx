ea8a059da392e6b59c736747782efd9190726bf0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-04 19:11:40 +0300

    update links

--- src/components/sections/Project/ProjectInfo.jsx
+++ src/components/sections/Project/ProjectInfo.jsx
@@ -1,4 +1,5 @@
-import { SectionWrapper, SectionTitle } from '../../';
+import { styled } from 'styled-components';
+import { SectionWrapper, SectionTitle, ButtonLink } from '../../';
 
 export default SectionWrapper(({ project }) => {
   const { id, title, subtitle, description, links, image, year, date, categories, tags, info } = project;
@@ -8,13 +9,12 @@ export default SectionWrapper(({ project }) => {
       <SectionTitle title='Project Details' />
       <div className='col-lg-8'>
         <div className='art-a art-card art-fluid-card'>
-          <h5 className='mb-15'>Description</h5>
-          <div className='mb-15'>{description}</div>
-          <div className='art-buttons-frame'>
-            <a href='https://www.behance.net/gallery/83236527/VIE' className='art-link art-color-link art-w-chevron' target='_blank'>
-              Read More
-            </a>
-          </div>
+          <DescriptionTitle className='mb-15'>Description</DescriptionTitle>
+          <Description className='mb-15'>{description}</Description>
+          <Links className='acc'>
+            {links.repo && <ButtonLink title='Repository' link={links.repo} icon='fa-brands fa-github' />}
+            {links.live && <ButtonLink title='Live Site' link={links.live} icon='fa-solid fa-globe' />}
+          </Links>
         </div>
       </div>
       <div className='col-lg-4'>
@@ -48,3 +48,25 @@ export default SectionWrapper(({ project }) => {
     </>
   );
 });
+
+const DescriptionTitle = styled.h5`
+  font-family: var(--f-primary);
+`;
+
+const Description = styled.p``;
+
+const Links = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+
+  a {
+    color: var(--c-accent-1);
+    flex-direction: row-reverse !important;
+    gap: 1rem;
+  }
+
+  i {
+    font-size: 1rem !important;
+  }
+`;
