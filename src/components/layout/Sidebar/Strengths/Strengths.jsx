e7b6be8012642241beef21699324569026534e69
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-04 19:43:24 +0300

    add rng to tags

--- src/components/layout/Sidebar/Strengths/Strengths.jsx
+++ src/components/layout/Sidebar/Strengths/Strengths.jsx
@@ -1,16 +1,16 @@
 import { motion } from "framer-motion";
 
 import { dataStore } from "../../../../store/dataStore";
-import { sidebarMotion } from "../../../../utils";
 import { Tag } from "../../../../styles";
+import { rng, sidebarMotion } from "../../../../utils";
 
 export const Strengths = () => {
   const data = dataStore((state) => state.sidebar.strengths);
   
   return (
-    <motion.div className='art-strengths p-15-15' {...sidebarMotion.strengths.container}>
+    <motion.div className='art-strengths p-15-15 acc' {...sidebarMotion.strengths.container}>
       {data.map((strength, index) => (
-        <Tag key={`sidebar-strenghts-${index}`} {...sidebarMotion.strengths.item}>{strength}</Tag>
+        <Tag key={`sidebar-strenghts-${index}`} {...sidebarMotion.strengths.item} $rng={rng(1, 5)}>{strength}</Tag>
       ))}
     </motion.div>
   );
