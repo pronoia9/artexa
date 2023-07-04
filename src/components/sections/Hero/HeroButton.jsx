6dd3128b6bfddc4320fe06e78f2d4221b365bb36
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-04 17:53:05 +0300

    use buttongradient component

--- src/components/sections/Hero/HeroButton.jsx
+++ src/components/sections/Hero/HeroButton.jsx
@@ -1,15 +1,12 @@
+import { ButtonGradient, Button } from '../..';
 import { dataStore } from '../../../store/dataStore';
-import { GradientButton } from '../../../styles/ButtonStyles';
-import { heroMotion } from '../../../utils';
 
 export const HeroButton = ({ handleClick }) => {
   const data = dataStore((state) => state.hero.button);
 
   return (
-    <GradientButton className='art-buttons-frame' {...heroMotion.button}>
-      <a href={data.url} className='art-btn art-btn-md' onClick={handleClick}>
-        <span>{data.text}</span>
-      </a>
-    </GradientButton>
+    <ButtonGradient>
+      <Button title={data.text} arrow={false} onClick={handleClick} />
+    </ButtonGradient>
   );
 };
