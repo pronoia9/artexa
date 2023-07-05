a6221de8271fac4fcb4a634e99b2331429da0053
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-05 22:57:52 +0300

    send index

--- src/components/components/Timeline/Timeline.jsx
+++ src/components/components/Timeline/Timeline.jsx
@@ -3,7 +3,7 @@ import { TimelineColumn } from '../..';
 export const Timeline = ({ titles, children, ...props }) => {
   return children ? (
     children.map((tl, index) => (
-      <TimelineColumn column={children.length || 2} title={titles[index] || ''} key={`timeline-column-${index}`} data={tl} />
+      <TimelineColumn column={children.length || 2} title={titles[index] || ''} key={`timeline-column-${index}`} data={tl} index={index} />
     ))
   ) : (
     <TimelineColumn {...props} /> // column={column || 2} title={title || ''} key={`timeline-column-${index}`} data={data}
