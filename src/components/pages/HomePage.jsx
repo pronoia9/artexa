56a8a3eada2d683066556e1d6f202b558ca7ca76
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-06 01:02:23 +0300

    update delays with the new system

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,12 +4,12 @@ const HomePage = () => {
   return (
     <>
       <Hero stagger={0.25} />
-      <Counters stagger={0.1} delay={1} />
-      <Services stagger={0.1} delay={1.25} />
-      <Projects stagger={0.25} delay={2} type='grid' limit={true} />
-      <History stagger={0.25} delay={4} />
-      <Technologies stagger={0.25} delay={5} type='swiper' />
-      <Contact stagger={0.25} delay={6} />
+      <Counters stagger={0.1} delay={0.75} />
+      <Services stagger={0.1} delay={0.75} />
+      <Projects stagger={0.25} delay={1} type='grid' limit={true} />
+      <History stagger={0.25} delay={1.5} />
+      <Technologies stagger={0.25} delay={2.25} type='swiper' />
+      <Contact stagger={0.25} delay={2.75} />
     </>
   );
 };
