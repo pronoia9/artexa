a26426aee71eef49fc5d069fd398599d2c24c4b4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-06 00:24:16 +0300

    update delays

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -6,11 +6,11 @@ const HomePage = () => {
     <>
       <Hero motion={heroMotion.container(0.75, 0.75)} />
       <Counters motion={countersMotion.container(0.1)} />
-      <Services motion={servicesMotion.container(0.25)} />
-      <Projects motion={projectsMotion.container(0.5, 1)} type='grid' limit={true} />
-      <History motion={historyMotion.container(0.5, 2)} />
-      <Technologies motion={technologiesMotion.container(0.25, 3)} type='swiper' />
-      <Contact motion={contactMotion.container(0.25, 4)} />
+      <Services motion={servicesMotion.container(0.1)} />
+      <Projects motion={projectsMotion.container(0.25, 0.5)} type='grid' limit={true} />
+      <History motion={historyMotion.container(0.25, 1.5)} />
+      <Technologies motion={technologiesMotion.container(0.25, 2.5)} type='swiper' />
+      <Contact motion={contactMotion.container(0.25, 3.5)} />
     </>
   );
 };
