263e590dbbd9ba110e58658f9e9dfee684048281
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-10 20:13:43 +0300

    format

--- src/components/components/SmoothScroll.jsx
+++ src/components/components/SmoothScroll.jsx
@@ -4,22 +4,16 @@ import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
 import { motion } from 'framer-motion';
 import { styled } from 'styled-components';
 
-export const SmoothScroll = (props) => {
+export const SmoothScroll = ({ options, ...props }) => {
   const scrollRef = useRef(null);
 
   useEffect(() => {
     Scrollbar.use(OverscrollPlugin);
-    Scrollbar.init(scrollRef.current, { damping: 0.5 });
+    Scrollbar.init(scrollRef.current, { damping: 0.5, ...options });
   });
 
   return (
-    <Container
-      ref={scrollRef}
-      className='art-scroll-frame'
-      damping={0.5}
-      plugins={{ overscroll: { effect: 'bounce' } }}
-      {...props}
-    >
+    <Container ref={scrollRef} className='art-scroll-frame' {...props}>
       {props.children}
     </Container>
   );
