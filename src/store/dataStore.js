9364ea609bd0d50968f2c94e948e3c0457fc57f1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-10 18:49:41 +0300

    remove unused asset imports

--- src/store/dataStore.js
+++ src/store/dataStore.js
@@ -1,18 +1,15 @@
 import { create } from 'zustand';
 
 import { 
+  heroAvatar, sidebarAvatar,
   // Footer
   footer1, footer2, footer3, footer4,
-  // Hero
-  heroBgBlue, heroBgGreen, heroBgPastel, heroBgPink, heroBgPurple, heroBgRed, heroBgYellow, heroAvatar,
   // History
   historyCei, historySc, historyTabWdb, historyZtmTcwd,
   // Hobbies
   baking, beanBagChairChill, dopPooping, gaming, keyboardSmash, loveThisWithCat, sticker2, sticker3, sticker,
   // Projects
   banarama, dallElyse, donutDelights, metaversus, neonRetroverse, niftyNoggin, nocturnalEnigma, organizedBliss, portfolio2023, portfolio, punyMath, sereneVistas, sprinkleSpectacular, stellarCountdown, teeTailor, westsum,
-  // Sidebar
-  sidebarAvatar,
   // Technologies
   techAnimatedCursor, techAxios, techBitmoji, techCountup, techEmailjs, techFancybox, techFontAwesome, techFormik, techFramerMotion, techGsap, techPoimandres, techProgressbar, techReactRouter, techReactThreeDrei, techReactThreeFiber, techSmoothScrollbar, techStyledComponents, techSwiper, techThreejs, techTypedjs, techVitejs,
  } from '../assets';
@@ -1097,7 +1094,7 @@ export const dataStore = create((set) => ({
       {
         title: 'Smooth Scrollbar',
         subtitle: '',
-        link: '',
+        link: 'https://idiotwu.github.io/smooth-scrollbar/',
         image: techSmoothScrollbar,
       },
       {
