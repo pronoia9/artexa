eafe6d216cce602269265ba54cc8b687dab1d401
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-13 23:05:36 +0300

    update/not really

--- src/components/components/Room/Effects.jsx
+++ src/components/components/Room/Effects.jsx
@@ -1,9 +1,11 @@
-import { Environment } from '@react-three/drei';
+import { Environment, Sky } from '@react-three/drei';
 import { EffectComposer, Bloom, DepthOfField } from '@react-three/postprocessing';
 
 export const Effects = () => {
   return (
     <>
+      {/* <Sky scale={1000} sunPosition={[2, 0.4, 10]} /> */}
+      {/* <fog attach='fog' args={['#ff5020', 5, 18]} /> */}
       <Environment preset='forest' />
       <EffectComposer>
         <Bloom intensity={1.0} luminanceThreshold={0.9} luminanceSmoothing={0.025} mipmapBlur={true} />
