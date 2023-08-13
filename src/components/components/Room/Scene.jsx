3cdebc83d6c456feef7df50c52a2a3b4b7f63f61
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-13 23:06:22 +0300

    remove leva controls

--- src/components/components/Room/Scene.jsx
+++ src/components/components/Room/Scene.jsx
@@ -1,24 +1,17 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, PerspectiveCamera, ScrollControls } from '@react-three/drei';
+import { OrbitControls, ScrollControls } from '@react-three/drei';
 
 import { Lights } from './Lights';
 import { Model } from './Model';
 import { Effects } from './Effects';
-import { useControls } from 'leva';
 
 export const Scene = () => {
-  const { scale, position, rotation } = useControls('model', {
-    scale: { value: 1 },
-    position: { value: [0, 0, 0], step: 0.1 },
-    rotation: { value: [0, 6.5, 0], step: 0.1 },
-  });
-
   return (
-    <Canvas dpr={[1, 2]} camera={{ position: [29, 14, 12], fov: 35 }} gl={{ antialias: true }}>
-      {/* <OrbitControls /> */}
+    <Canvas dpr={[1, 2]} camera={{ fov: 35 }} gl={{ antialias: true }} shadows={false}>
+      <OrbitControls />
       <Lights />
-      <ScrollControls>
-        <Model scale={scale} position={position} rotation={rotation} />
+      <ScrollControls pages={6} damping={0.1}>
+        <Model />
       </ScrollControls>
       <Effects />
     </Canvas>
