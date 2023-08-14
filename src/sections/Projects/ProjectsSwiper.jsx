7df565151b608feef6bbd1f1baa8431ade68792d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-14 22:11:35 +0300

    move all components to components folder

--- src/sections/Projects/ProjectsSwiper.jsx
+++
@@ -1,9 +0,0 @@
-import { Swiper } from '../../components/Swiper';
-import { dataStore } from '../../utils/dataStore';
-import { projectsMotion } from '@/utils';
-
-export const ProjectsSwiper = (props) => {
-  const data = dataStore((state) => state.projects.projects);
-
-  return <Swiper section='project' data={data} autoplay={true} navProps={projectsMotion.navigation} {...props} {...projectsMotion.swiper} />;
-};
