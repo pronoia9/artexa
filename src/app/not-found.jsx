a3e1eb9d83ce513f87787fe3cdf3b750757dd094
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-15 00:10:59 +0300

    add shallow to link

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,27 +1,3 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2023 Nate Watson (https://codepen.io/nw/pen/WQmxYY)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 'use client';
 
 import { useLayoutEffect, useRef } from 'react';
@@ -89,9 +65,7 @@ export default function Page() {
     onResize();
     updateParticles();
     window.addEventListener('resize', onResize);
-    return () => {
-      window.removeEventListener('resize', onResize);
-    };
+    return () => { window.removeEventListener('resize', onResize); };
   }, []);
 
   return (
@@ -103,7 +77,7 @@ export default function Page() {
           <br />
           That page has gone missing.
         </h1>
-        <Link className='home-link' href='/'>
+        <Link href='/' shallow className='home-link'>
           Hitch a ride back home.
         </Link>
       </div>
@@ -116,8 +90,6 @@ export default function Page() {
     </Container>
   );
 }
-
-/************  animation  ***********/
 
 /*************  styles  ************/
 const Container = styled(motion.section)`
@@ -285,3 +257,27 @@ const Container = styled(motion.section)`
     }
   }
 `;
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2023 Nate Watson (https://codepen.io/nw/pen/WQmxYY)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
