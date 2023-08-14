b73dd081ebfbdb710424ed2d440976797c8e6af5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-14 22:11:01 +0300

    add pagewrapper again

---
+++ src/components/hoc/PageWrapper.jsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { styled } from 'styled-components';
+import { motion } from 'framer-motion';
+
+import { Background, Footer, SmoothScroll } from '@/components';
+import { dataStore, pageWrapperMotion, rem } from '@/utils';
+
+export const PageWrapper = (Component, idName) =>
+  function HOC(props) {
+    const { curtainEnabled, curtainClose } = dataStore((state) => ({
+      curtainEnabled: state.curtainEnabled,
+      curtainClose: state.curtainClose,
+    }));
+
+    return (
+      <Content id={`${idName}-page`} className='art-content' $curtainEnabled={curtainEnabled} onClick={() => curtainClose()} {...pageWrapperMotion()}>
+        <Curtain className='art-curtain' $curtainEnabled={curtainEnabled} />
+        <Background />
+        <SmoothScroll>
+          <Component {...props} />
+          {idName !== 'not-found' && <Footer />}
+        </SmoothScroll>
+      </Content>
+    );
+  };
+
+const Content = styled(motion.div)`
+  position: relative;
+  width: 100vw;
+  height: calc(100vh - ${rem(30)});
+  /* padding-right: ${rem(80)}; */
+  padding-right: ${rem(75)};
+  overflow: hidden;
+  transform: ${({ $curtainEnabled }) => $curtainEnabled && `translateX(${rem(-150)})`};
+  transition: 0.55s ease-in-out;
+
+  @media (max-width: ${rem(920)}) {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    padding-right: 0;
+    padding-top: ${rem(70)};
+    transform: ${({ $curtainEnabled }) => $curtainEnabled && 'none'};
+  }
+
+  @media (max-width: 230px) {
+    padding-right: 0;
+  }
+`;
+
+const Curtain = styled.div`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(30, 30, 40, 0.88);
+  opacity: ${({ $curtainEnabled }) => ($curtainEnabled ? 0.7 : 0)};
+  pointer-events: ${({ $curtainEnabled }) => ($curtainEnabled ? 'all' : 'none')};
+  z-index: 9;
+  transition: 0.55s ease-in-out;
+
+  @media (max-width: ${rem(920)}) {
+    pointer-events: ${({ $curtainEnabled }) => $curtainEnabled && 'all'};
+    opacity: ${({ $curtainEnabled }) => $curtainEnabled && 1};
+  }
+`;
