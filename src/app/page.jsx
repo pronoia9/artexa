580082c84d88636f1f8882a8b5b463fd3963fd8b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-14 23:26:15 +0300

    enable all components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,9 +8,9 @@ export default function Page() {
       <Hero stagger={0.25} />
       <Counters stagger={0.1} delay={0.75} />
       <Services stagger={0.1} delay={0.75} />
-      {/* <Projects stagger={0.25} delay={1} type='grid' limit={true} /> */}
+      <Projects stagger={0.25} delay={1} type='grid' limit={true} />
       <History stagger={0.25} delay={1.5} />
-      {/* <Technologies stagger={0.25} delay={2.25} type='swiper' /> */}
+      <Technologies stagger={0.25} delay={2.25} type='swiper' />
       <Contact stagger={0.25} delay={2.75} />
     </>
   );
