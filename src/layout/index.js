61d14d24689f27f980e63def982a9658cd486ee6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-14 19:24:21 +0300

    move layout components + add index

---
+++ src/layout/index.js
@@ -0,0 +1,32 @@
+// Footer
+import Footer from './Footer/Footer';
+// Navbar
+import { Navbar } from './Navbar/Navbar';
+import { NavbarButton } from './Navbar/NavbarButton';
+import { NavbarCurrentPage } from './Navbar/NavbarCurrentPage';
+import { NavbarList } from './Navbar/NavbarList';
+import { NavbarListItem } from './Navbar/NavbarListItem';
+import { ThemeButton } from './Navbar/ThemeButton';
+// Sidebar
+import { Sidebar } from './Sidebar/Sidebar';
+import { HardSkills } from './Sidebar/HardSkills/HardSkills';
+import { HardSkillsItem } from './Sidebar/HardSkills/HardSkillsItem';
+import { Info } from './Sidebar/Info/Info';
+import { InfoItem } from './Sidebar/Info/InfoItem';
+import { Languages } from './Sidebar/Languages/Languages';
+import { LanguagesItem } from './Sidebar/Languages/LanguagesItem';
+import { Profile } from './Sidebar/Profile/Profile';
+import { ProfileAvatar } from './Sidebar/Profile/ProfileAvatar';
+import { ProfileName } from './Sidebar/Profile/ProfileName';
+import { ProfilePost } from './Sidebar/Profile/ProfilePost';
+import { Resume } from './Sidebar/Resume/Resume';
+import { Socials } from './Sidebar/Socials/Socials';
+import { SocialsItem } from './Sidebar/Socials/SocialsItem';
+import { SoftSkills } from './Sidebar/SoftSkills/SoftSkills';
+import { Strengths } from './Sidebar/Strengths/Strengths';
+
+export {
+  Footer,
+  Navbar, NavbarButton, NavbarCurrentPage, NavbarList, NavbarListItem, ThemeButton,
+  Sidebar, HardSkills, HardSkillsItem, Info, InfoItem, Languages, LanguagesItem, Profile, ProfileAvatar, ProfileName, ProfilePost, Resume, Socials, SocialsItem, SoftSkills, Strengths,
+};
