ba50e2e0bd2eee9b5cbe72bc37357f8aea6ff5e4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-15 22:46:27 +0300

    add right arrow svg

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -32,7 +32,7 @@ export const ButtonLink = ({ link, title, arrow = true, children, icon, ...props
     <Link href={link} className='art-link art-color-link art-w-chevron acc' {...props} onClick={handleClick}>
       {title}
       {children}
-      {(arrow || icon) && <SVGs type={icon} height={9} />}
+      {(icon || arrow) && <SVGs type={icon || 'right'} height={9} />}
     </Link>
   );
 };
