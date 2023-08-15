43eadf303fdc750817aa1a6182adcbd8552ca2d2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-15 17:56:10 +0300

    add svg

--- src/components/layout/Sidebar/Resume.jsx
+++ src/components/layout/Sidebar/Resume.jsx
@@ -4,13 +4,17 @@ import { styled } from 'styled-components';
 import { motion } from 'framer-motion';
 
 import { sidebarMotion, sidebar } from '@/utils';
+import { SVGs } from '@/components';
 
 export const Resume = () => {
   const data = sidebar.resume;
   return (
     <Container className='art-links-frame p-15-15' {...sidebarMotion.resume.container}>
       <a href={data.url} className='art-link art-color-link' download>
-        <motion.span {...sidebarMotion.resume.text}>{data.title}</motion.span> <motion.i {...sidebarMotion.resume.icon} className='fas fa-download' />
+        <motion.span {...sidebarMotion.resume.text}>{data.title} </motion.span>
+        <motion.span {...sidebarMotion.resume.icon}>
+          <SVGs type='download' height={10} />
+        </motion.span>
       </a>
     </Container>
   );
