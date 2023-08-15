6d22a2c3af2d2ebc1c4b7053d70dd199e33c83ed
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-15 17:43:50 +0300

    update ProgressbarInstance

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -75,15 +75,15 @@ export const getProjectsCount = (rows = 3, cols = 2) => {
 }
 
 //----------------------------- PROGRESSBARS -----------------------------//
-export const ProgressbarInstance = ({ type = 'circle', level, index, hide, duration = 5000, delay = 750 }) => {
+export const ProgressbarInstance = ({ type = 'circle', level, index, hide, duration, delay }) => {
   const id = `#${type}prog${index}`;
   switch (type) {
     case 'circle':
       return new ProgressBar.Circle(id, {
         strokeWidth: 7,
         easing: 'easeInOut',
-        duration: 1400,
-        delay: 3400 + Number(index) * 100,
+        duration: duration || 1400,
+        delay: delay || 3400 + Number(index) * 100,
         trailWidth: 7,
         step: (state, bar) => { bar.setText(!hide ? `${level}%`: ''); },
       });
@@ -91,8 +91,8 @@ export const ProgressbarInstance = ({ type = 'circle', level, index, hide, durat
       return new ProgressBar.Line(id, {
         strokeWidth: 1.72,
         easing: 'easeInOut',
-        duration: 1400,
-        delay: 4700 + Number(index) * 100,
+        duration: duration || 1400,
+        delay: delay || 4700 + Number(index) * 100,
         trailWidth: 7,
         svgStyle: { width: '100%', height: '100%' },
         step: (state, bar) => { bar.setText(!hide ? `${level} %` : ''); },
@@ -101,8 +101,8 @@ export const ProgressbarInstance = ({ type = 'circle', level, index, hide, durat
       return new ProgressBar.Line(id, {
         strokeWidth: 1.7,
         easing: 'easeInOut',
-        duration,
-        delay,
+        duration: duration || 5000,
+        delay: delay || 750,
         trailWidth: 1.7,
         svgStyle: { width: '100%', height: '100%' },
         step: (state, bar) => { bar.setText(Math.round(bar.value() * 100) + ' %'); },
