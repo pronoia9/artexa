731059846d95c4f73ef38bedd6ea3f82f863ff36
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-16 21:37:40 +0300

    add hobbies card image styles

--- src/components/sections/About/HobbiesCard.jsx
+++ src/components/sections/About/HobbiesCard.jsx
@@ -7,10 +7,10 @@ import { HoverExpand } from '@/components';
 
 export const HobbiesCard = ({ title, image }) => {
   return (
-    <Frame className='art-a art-hobbies-item-frame acc'>
+    <Frame className='art-a art-hobbies-item-frame'>
       <Item data-fancybox='hobbies' href={image} className='art-a art-hobbies-item-frame art-horizontal'>
         <img src={image} alt={title} />
-        <HoverExpand />
+        <HoverExpand center={true} />
       </Item>
     </Frame>
   );
@@ -32,7 +32,29 @@ const Item = styled.div`
   padding: 0;
   box-shadow: none;
 
+  display: block;
+  width: 100%;
+  outline: inherit;
+  overflow: hidden;
+  position: relative;
+  padding-bottom: 60%;
+
   img {
-    object-fit: scale-down !important;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    object-fit: scale-down;
+    object-position: center;
+    filter: brightness(85%) blur(0);
+    transition: 0.4s ease-in-out;
+  }
+
+  &:hover {
+    img {
+      transform: scale(1.07);
+      filter: brightness(100%) blur(0);
+    }
   }
 `;
