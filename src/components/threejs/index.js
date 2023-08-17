e166ec7a05c7bcca5198061750322d0a3c69fefe
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-17 17:16:21 +0300

    add init files and assets

---
+++ src/components/threejs/index.js
@@ -0,0 +1,10 @@
+import { Experience } from './Experience';
+import { Model } from './Model';
+
+const bakedDay = '@/components/threejs/assets/bakedDay.jpg';
+const bakedNeutral = '@/components/threejs/assets/bakedNeutral.jpg';
+const bakedNight = '@/components/threejs/assets/bakedNight.jpg';
+const lightMap = '@/components/threejs/assets/lightMap.jpg';
+const roomModel = '@/components/threejs/assets/brunos-room-transformed.glb';
+
+export { bakedDay, bakedNeutral, bakedNight, lightMap, roomModel, Experience, Model };
