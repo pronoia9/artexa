071e20ee0ebf0aac565ab08c6c685d00e4918ef1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-17 17:23:54 +0300

    fix new paths

--- src/components/threejs/index.js
+++ src/components/threejs/index.js
@@ -1,10 +1,10 @@
 import { Experience } from './Experience';
 import { Model } from './Model';
 
-const bakedDay = '@/components/threejs/assets/bakedDay.jpg';
-const bakedNeutral = '@/components/threejs/assets/bakedNeutral.jpg';
-const bakedNight = '@/components/threejs/assets/bakedNight.jpg';
-const lightMap = '@/components/threejs/assets/lightMap.jpg';
-const roomModel = '@/components/threejs/assets/brunos-room-transformed.glb';
+const bakedDay = '/3d/bakedDay.jpg';
+const bakedNeutral = '/3d/bakedNeutral.jpg';
+const bakedNight = '/3d/bakedNight.jpg';
+const lightMap = '/3d/lightMap.jpg';
+const roomModel = '/3d/brunos-room-transformed.glb';
 
 export { bakedDay, bakedNeutral, bakedNight, lightMap, roomModel, Experience, Model };
