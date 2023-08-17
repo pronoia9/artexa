fc7359d9939334b1033936908e9e9ff87bb3669a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-17 15:31:25 +0300

    add suppressHydrationWarning

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
   }, []);
 
   return (
-    <html>
+    <html lang='en' suppressHydrationWarning={true}>
       <StyledComponentsRegistry>
         <ThemeProvider theme={getThemeObject(theme)}>
           <ThemeProvider theme={getThemeObject(accent)}>
