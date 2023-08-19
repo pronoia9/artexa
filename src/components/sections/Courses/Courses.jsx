c71b7ae27657e82c4138a91f685f83ed2ff1b8f5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-19 19:31:41 +0300

    add check

--- src/components/sections/Courses/Courses.jsx
+++ src/components/sections/Courses/Courses.jsx
@@ -1,6 +1,7 @@
 import { SectionWrapper, Timeline } from '@/components';
-import { courses as data } from '@/utils/';
+import { courses as data } from '@/utils';
 
 export default SectionWrapper(() => {
+  if (!data) return;
   return data.map((col, index) => <Timeline key={`courses-${index}-${col?.title}`} {...col} column={2} />);
 }, 'courses');
