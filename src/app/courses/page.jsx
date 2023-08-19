e5d80e7fef316d90eb626ce975545454d58c2c03
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-19 20:35:55 +0300

    rerererenable content

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,9 +1,9 @@
 import { Courses } from '@/components';
-// TODO: FIX THIS SHIT ASDA DASDASDAFASfm;sd,gm;sdlf
+
 export default function Page() {
   return (
     <>
-      {/* <Courses /> */}
+      <Courses />
     </>
   );
 }
