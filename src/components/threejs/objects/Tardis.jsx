cc030fcf5f1a436bcfee5a47911799c8b7808d56
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-21 00:59:32 +0300

    add random rotation to tardis

--- src/components/threejs/objects/Tardis.jsx
+++ src/components/threejs/objects/Tardis.jsx
@@ -1,17 +1,38 @@
 import { Float } from '@react-three/drei';
 import { motion } from 'framer-motion-3d';
 
-import { sceneMotion } from '@/utils';
+import { rngInRange, sceneMotion } from '@/utils';
+import { useEffect, useState } from 'react';
 
 export const Tardis = ({ nodes, materials, ...props }) => {
+  const [rotate, setRotate] = useState([-0.87, null]);
+
+  useEffect(() => {
+    let timeout;
+    const turn = () => {
+      setRotate((prev) => [prev[1] || -0.87, parseFloat(prev) + rngInRange(-Math.PI, Math.PI) + Math.random()]);
+      timeout = setTimeout(turn, (5 + Math.random() * 5) * 1000);
+    };
+    turn();
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <Float>
+    <Float
+      peed={1} // Animation speed, defaults to 1
+      rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+      floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+      floatingRange={[-0.2, 0.2]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+    >
       <motion.group
+        key={`tardis-group-rotation-${rotate[1]}`}
         name='Tardis'
         position={[2.32, 1.84, 4.82]}
         rotation={[-3.13, -0.87, -3.08]}
-        scale={2.63}
-        {...sceneMotion.bakedMesh(0, 0, { visible: { scale: 2.63 } })}
+        {...sceneMotion.bakedMesh(0, 0, rotate[1] ? {
+          hidden: { scale: 1, rotateY: -0.87 }, 
+          visible: { scale: 1, rotateY: rotate[1], transition: { type: 'ease', duration: '2' } }
+        } : {})}
         {...props}
       >
         <mesh name='Tardis_1' geometry={nodes.Tardis_1.geometry} material={materials.blue_wood} />
