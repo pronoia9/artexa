f230c0fbbc44e35dea5452b6502017c4c233c356
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-22 18:36:49 +0300

    add changing theme + fix texture props/flipYs

--- src/components/threejs/materials/BakedMesh.jsx
+++ src/components/threejs/materials/BakedMesh.jsx
@@ -1,27 +1,54 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { MeshBasicMaterial } from 'three';
+import { useEffect, useRef, useState } from 'react';
+import { MeshBasicMaterial, SRGBColorSpace } from 'three';
 import { extend } from '@react-three/fiber';
 import { useTexture } from '@react-three/drei';
+import { useControls, button } from 'leva';
 import { motion } from 'framer-motion-3d';
 
-import { BakedMaterial } from '@/components/threejs';
+// import { BakedMaterial } from '@/components/threejs';
 import { dataStore, isDarkTheme, rngInRange, sceneMotion } from '@/utils';
 
 extend({ MeshBasicMaterial });
 
 export const BakedMesh = ({ showCube, variants, children, ...props }) => {
-  const { theme } = dataStore((store) => ({ theme: store.theme }));
-  const bakedTextureDay = useTexture('/3d/bakedDay.jpg'),
-    bakedTextureNight = useTexture('/3d/bakedNight.jpg');
-  const [bakedTexture, setBakedTexture] = useState(bakedTextureNight);
+  const meshRef = useRef(),
+    materialRef = useRef();
+  const { theme, toggleTheme } = dataStore((store) => ({ theme: store.theme, toggleTheme: store.toggleTheme }));
+  const dayTexture = useTexture('/3d/bakedDay.jpg'),
+    nightTexture = useTexture('/3d/bakedNight.jpg'),
+    lightMap = useTexture('/3d/lightMap.jpg');
 
-  useEffect(() => void setBakedTexture(isDarkTheme(theme) ? bakedTextureNight : bakedTextureDay), [theme]);
+  dayTexture.flipY = false;
+  dayTexture.colorSpace = SRGBColorSpace;
+  nightTexture.flipY = false;
+  nightTexture.colorSpace = SRGBColorSpace;
+  lightMap.flipY = false;
+
+  const updateMaterials = () => {
+    materialRef.current.map.needsUpdate = true;
+    materialRef.current.map.needsPMREUpdate = true;
+  };
+
+  const options = useControls('Texture', {
+    'Toggle Time': button(() => {
+      toggleTheme();
+      updateMaterials();
+    }),
+    // 'Toggle Light Map': button(() => { }),
+    // 'Light Intensity': { value: 50, min: 0, max: 100, step: 0.1 },
+  });
 
   return (
-    <motion.mesh {...sceneMotion.bakedMesh(rngInRange(0.1, 0.25), rngInRange(0.1, 0.25), variants)} {...props}>
-      {<meshBasicMaterial key={bakedTexture} map={bakedTexture} map-flipY={false} />}
+    <motion.mesh ref={meshRef} {...sceneMotion.bakedMesh(rngInRange(0.1, 0.25), rngInRange(0.1, 0.25), variants)} castShadow receiveShadow {...props}>
+      <meshBasicMaterial
+        ref={materialRef}
+        key={`${theme}-${options}`}
+        map={isDarkTheme(theme) ? nightTexture : dayTexture}
+        // lightMap={lightMap}
+        // lightMapIntensity={options['Light Intensity']}
+      />
       {/* <BakedMaterial /> */}
       {children}
     </motion.mesh>
