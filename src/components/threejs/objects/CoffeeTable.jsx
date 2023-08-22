269d355d1ac8234a46be2ce1d2aa17dc1401c4df
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-22 20:14:16 +0300

    add google home leds + animation

--- src/components/threejs/objects/CoffeeTable.jsx
+++ src/components/threejs/objects/CoffeeTable.jsx
@@ -1,8 +1,23 @@
 'use client';
 
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useTexture } from '@react-three/drei';
+
 import { BakedMesh } from '@/components/threejs';
 
 export const CoffeeTable = ({ nodes, ...props }) => {
+  const ledRefs = useRef([]);
+  const ledMask = useTexture('/3d/googleHomeLedMask.png');
+  const ledColors = ['#196aff', '#ff0000', '#ff5d00', '#7db81b'];
+
+  useFrame((state) => {
+    const elapsedTime = state.clock.elapsedTime;
+    ledRefs.current.forEach((item, index) => {
+      item.opacity = Math.sin(elapsedTime * 2 - index * 0.5) * 0.5 + 0.5;
+    });
+  });
+
   return (
     <BakedMesh
       name='Coffee_Table'
@@ -13,35 +28,49 @@ export const CoffeeTable = ({ nodes, ...props }) => {
     >
       <BakedMesh name='Controller' geometry={nodes.Controller.geometry} material={nodes.Controller.material} position={[-0.17, 0.22, -0.39]} />
 
-      <BakedMesh name='Google_Home' geometry={nodes.Google_Home.geometry} material={nodes.Google_Home.material} position={[0.51, 0.21, 0.47]}>
+      <BakedMesh
+        name='Google_Home'
+        geometry={nodes.Google_Home.geometry}
+        material={nodes.Google_Home.material}
+        position={[0.51, 0.21, 0.47]}
+        rotation={[0, 0, 1]}
+      >
         <mesh
           name='Google_Home_Leds001'
           geometry={nodes.Google_Home_Leds001.geometry}
           material={nodes.Google_Home_Leds001.material}
           position={[-0.04, 0.04, 0]}
           rotation={[0, 0, 0.12]}
-        />
+        >
+          <meshBasicMaterial ref={(ref) => (ledRefs.current[0] = ref)} color={ledColors[0]} transparent alphaMap={ledMask} />
+        </mesh>
         <mesh
           name='Google_Home_Leds002'
           geometry={nodes.Google_Home_Leds002.geometry}
           material={nodes.Google_Home_Leds002.material}
           position={[0.04, 0.04, 0]}
           rotation={[-Math.PI, 0, -3.02]}
-        />
+        >
+          <meshBasicMaterial ref={(ref) => (ledRefs.current[1] = ref)} color={ledColors[1]} transparent alphaMap={ledMask} />
+        </mesh>
         <mesh
           name='Google_Home_Leds003'
           geometry={nodes.Google_Home_Leds003.geometry}
           material={nodes.Google_Home_Leds003.material}
           position={[0.01, 0.04, 0]}
           rotation={[-Math.PI, 0, -3.1]}
-        />
+        >
+          <meshBasicMaterial ref={(ref) => (ledRefs.current[2] = ref)} color={ledColors[2]} transparent alphaMap={ledMask} />
+        </mesh>
         <mesh
           name='Google_Home_Leds004'
           geometry={nodes.Google_Home_Leds004.geometry}
           material={nodes.Google_Home_Leds004.material}
           position={[-0.01, 0.04, 0]}
           rotation={[0, 0, 0.04]}
-        />
+        >
+          <meshBasicMaterial ref={(ref) => (ledRefs.current[3] = ref)} color={ledColors[3]} transparent alphaMap={ledMask} />
+        </mesh>
       </BakedMesh>
     </BakedMesh>
   );
