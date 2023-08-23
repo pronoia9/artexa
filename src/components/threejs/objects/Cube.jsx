4b281a9c17772b9a38e2e99fcb1edb72d848424b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-23 18:16:49 +0300

    change to materials material

--- src/components/threejs/objects/Cube.jsx
+++ src/components/threejs/objects/Cube.jsx
@@ -2,20 +2,13 @@
 
 import { motion } from 'framer-motion-3d';
 
-import { colors } from '@/styles';
 import { dataStore, isDarkTheme } from '@/utils';
 
 export const Cube = ({ nodes, materials, children, ...props }) => {
-  const { theme, accent } = dataStore((store) => ({ theme: store.theme }));
+  const { theme } = dataStore((store) => ({ theme: store.theme }));
 
   return (
     <motion.group name='Cube' position={[0, 2.44, 0]} scale={0.25} {...props}>
-      {/* <mesh name='Cube_1' geometry={nodes.Cube_1.geometry} material={nodes.Cube_1.material}>
-        <meshBasicMaterial color={colors[accent ? accent : 'purple']['accent1']} />
-      </mesh>
-      <mesh name='Cube_2' geometry={nodes.Cube_2.geometry} material={nodes.Cube_2.material}>
-        <meshBasicMaterial color={colors[accent ? accent : 'purple']['accent2']} />
-      </mesh> */}
       <mesh name='Cube_1' geometry={nodes.Cube_1.geometry} material={materials[`Cube ${isDarkTheme(theme) ? 'Night' : 'Day'}.002`]} />
       <mesh name='Cube_2' geometry={nodes.Cube_2.geometry} material={materials[`Cube ${isDarkTheme(theme) ? 'Night' : 'Day'}.001`]} />
       {children}
