ce08e809f5c8d01c4f0bf7458bfeeb758cbc2677
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-23 18:16:17 +0300

    send materials to cube

--- src/components/threejs/objects/Cube.jsx
+++ src/components/threejs/objects/Cube.jsx
@@ -3,19 +3,21 @@
 import { motion } from 'framer-motion-3d';
 
 import { colors } from '@/styles';
-import { dataStore } from '@/utils';
+import { dataStore, isDarkTheme } from '@/utils';
 
-export const Cube = ({ nodes, children, ...props }) => {
-  const { accent } = dataStore((store) => ({ theme: store.accent }));
+export const Cube = ({ nodes, materials, children, ...props }) => {
+  const { theme, accent } = dataStore((store) => ({ theme: store.theme }));
 
   return (
     <motion.group name='Cube' position={[0, 2.44, 0]} scale={0.25} {...props}>
-      <mesh name='Cube_1' geometry={nodes.Cube_1.geometry} material={nodes.Cube_1.material}>
+      {/* <mesh name='Cube_1' geometry={nodes.Cube_1.geometry} material={nodes.Cube_1.material}>
         <meshBasicMaterial color={colors[accent ? accent : 'purple']['accent1']} />
       </mesh>
       <mesh name='Cube_2' geometry={nodes.Cube_2.geometry} material={nodes.Cube_2.material}>
         <meshBasicMaterial color={colors[accent ? accent : 'purple']['accent2']} />
-      </mesh>
+      </mesh> */}
+      <mesh name='Cube_1' geometry={nodes.Cube_1.geometry} material={materials[`Cube ${isDarkTheme(theme) ? 'Night' : 'Day'}.002`]} />
+      <mesh name='Cube_2' geometry={nodes.Cube_2.geometry} material={materials[`Cube ${isDarkTheme(theme) ? 'Night' : 'Day'}.001`]} />
       {children}
     </motion.group>
   );
