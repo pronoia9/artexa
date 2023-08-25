2234ea50990be3a0cafd404f45bbfab68be0bbc1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-25 18:51:29 +0300

    remove stuff leftover from wobble variant

--- src/components/threejs/materials/BakedMesh.jsx
+++ src/components/threejs/materials/BakedMesh.jsx
@@ -1,21 +1,17 @@
 'use client';
 
-import { useRef } from 'react';
 import { MeshBasicMaterial } from 'three';
 import { extend } from '@react-three/fiber';
 import { motion } from 'framer-motion-3d';
 
 import { BakedMaterial } from '@/components/threejs';
-import { dataStore, isDarkTheme, rngInRange, sceneMotion } from '@/utils';
+import { rngInRange, sceneMotion } from '@/utils';
 
 extend({ MeshBasicMaterial });
 
 export const BakedMesh = ({ showCube, variants, children, ...props }) => {
-  const { theme } = dataStore((store) => ({ theme: store.theme }));
-  const meshRef = useRef(), materialRef = useRef();
-
   return (
-    <motion.mesh ref={meshRef} {...sceneMotion.bakedMesh(rngInRange(0.1, 0.25), rngInRange(0.1, 0.25), variants)} {...props}>
+    <motion.mesh {...sceneMotion.bakedMesh(rngInRange(0.1, 0.25), rngInRange(0.1, 0.25), variants)} {...props}>
       <BakedMaterial />
       {children}
     </motion.mesh>
