c038ca56621c89a97693e90417e473565db07381
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-25 17:26:55 +0300

    move zorua

--- src/components/threejs/objects/PetBed.jsx
+++ src/components/threejs/objects/PetBed.jsx
@@ -9,7 +9,7 @@ import { sceneMotion } from '@/utils';
 export const PetBed = ({ nodes, materials, ...props }) => {
   return (
     <BakedMesh name='Pet_Bed' geometry={nodes.Pet_Bed.geometry} material={nodes.Pet_Bed.material} position={[1.12, 0.17, 2.83]} {...props}>
-      <motion3d.group name='Zorua' position={[0.09, 0.03, -0.01]} rotation={[-2.42, 0.78, 2.59]} {...sceneMotion.bakedMesh()}>
+      <motion3d.group name='Zorua' position={[-0.09, 0.03, -0.05]} rotation={[-2.46, 0.72, 2.65]} {...sceneMotion.bakedMesh()}>
         <mesh name='Zorua_1' geometry={nodes.Zorua_1.geometry} material={materials['Zorua #35']} />
         <mesh name='Zorua_2' geometry={nodes.Zorua_2.geometry} material={materials['Zorua #36']} />
         <group name='Zorua_Head' position={[0, 0.19, 0.14]}>
