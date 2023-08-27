4eabf47f7813ff344ddaf6c3c5453a6d3c09c11f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-27 17:59:24 +0300

    change cube colors

--- src/components/threejs/objects/Cube.jsx
+++ src/components/threejs/objects/Cube.jsx
@@ -10,10 +10,10 @@ export const Cube = ({ nodes, materials, children, ...props }) => {
   return (
     <motion.group name='Cube' position={[0, 2.44, 0]} scale={0.25} {...props}>
       <mesh name='Cube001' geometry={nodes.Cube001.geometry} material={nodes.Cube001.material}>
-        <meshStandardMaterial color={`#${isDarkTheme(theme) ? '862327' : 'BA3E3C'}`} />
+        <meshStandardMaterial color={`#${isDarkTheme(theme) ? 'B63C3C' : 'CF504A'}`} />
       </mesh>
       <mesh name='Cube001_1' geometry={nodes.Cube001_1.geometry} material={nodes.Cube001_1.material}>
-        <meshStandardMaterial color={`#${isDarkTheme(theme) ? 'C1A3A3' : 'FFF3DF'}`} />
+        <meshStandardMaterial color={`#${isDarkTheme(theme) ? 'FFF3FF' : 'FFF3DD'}`} />
       </mesh>
       {children}
     </motion.group>
