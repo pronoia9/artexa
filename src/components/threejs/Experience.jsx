13aae5b44477a271f16f9031c191e1f5b1f4f042
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-27 16:46:18 +0300

    uninstall postprocessing

--- src/components/threejs/Experience.jsx
+++ src/components/threejs/Experience.jsx
@@ -2,8 +2,6 @@
 
 import { Suspense } from 'react';
 import { ScrollControls, Environment, Stars, useProgress, Sky, Cloud } from '@react-three/drei';
-// import { BlurPass, Resizer, KernelSize, Resolution } from 'postprocessing';
-// import { EffectComposer, Bloom } from '@react-three/postprocessing';
 // import { useControls } from 'leva';
 import { styled } from 'styled-components';
 import { motion } from 'framer-motion';
@@ -60,31 +58,6 @@ const Effects = () => {
       </group>
 
       <Environment files='/3d/christmas_photo_studio_04_1k.hdr' />
-      {/* <EffectComposer
-        // enabled={boolean}
-        // depthBuffer={boolean}
-        // disableNormalPass={boolean}
-        // stencilBuffer={boolean}
-        autoClear={true}
-        // multisampling={number}
-        // frameBufferType={TextureDataType}
-        // For effects that support DepthDownsamplingPass
-        resolutionScale={Resolution.AUTO_SIZE}
-        // renderPriority={number}
-        // camera={THREE.Camera}
-        // scene={THREE.Scene}
-      >
-        <Bloom
-        intensity={0.25} // The bloom intensity. Default is 1
-        // luminanceThreshold={0.9} // luminance threshold. Raise this value to mask out darker elements in the scene.
-        // luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
-        // mipmapBlur={false} // Enables or disables mipmap blur.
-        // blurPass={undefined} // A blur pass.
-        // kernelSize={KernelSize.LARGE} // blur kernel size
-        // resolutionX={Resolution.AUTO_SIZE} // The horizontal resolution.
-        // resolutionY={Resolution.AUTO_SIZE} // The vertical resolution.
-        />
-      </EffectComposer> */}
     </>
   );
 };
