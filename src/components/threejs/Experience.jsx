d7fc26f35522920e464e941d00cdef14bec29c21
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-29 18:29:07 +0300

    move effects, add new components

--- src/components/threejs/Experience.jsx
+++ src/components/threejs/Experience.jsx
@@ -1,14 +1,13 @@
 'use client';
 
 import { Suspense } from 'react';
-import { ScrollControls, Environment, Stars, useProgress, Sky, Cloud } from '@react-three/drei';
-// import { useControls } from 'leva';
+import { ScrollControls, useProgress } from '@react-three/drei';
 import { styled } from 'styled-components';
 import { motion } from 'framer-motion';
 import { MotionCanvas } from 'framer-motion-3d';
 
-import { Extend, Room } from '@/components/threejs';
-import { dataStore, isDarkTheme, sceneMotion } from '@/utils';
+import { Extend, Camera, Room, Bloom, Emissives, Effects } from '@/components/threejs';
+import { sceneMotion } from '@/utils';
 
 export const Experience = () => {
   // const { active, progress, errors, item, loaded, total } = useProgress();
@@ -19,7 +18,11 @@ export const Experience = () => {
         <Extend />
         <Suspense fallback={null}>
           <ScrollControls pages={3}>
+            <Camera />
             <Room />
+            <Bloom>
+              <Emissives />
+            </Bloom>
           </ScrollControls>
           <Effects />
         </Suspense>
@@ -29,35 +32,8 @@ export const Experience = () => {
 };
 
 const Container = styled(motion.div)`
-  &,
-  canvas {
+  &, canvas {
     width: 100%;
     height: 100%;
   }
 `;
-
-const Effects = () => {
-  const { theme } = dataStore((state) => ({ theme: state.theme }));
-
-  return (
-    <>
-      <group key={`scene_effects-${theme}`}>
-        {isDarkTheme(theme) ? (
-          <>
-            <color attach='background' args={['#1e1e28']} />
-            <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
-          </>
-        ) : (
-          <>
-            <Sky sunPosition={[100, 20, 100]} />
-            <group position={[-9.1, -4.1, -14.1]} /*{...useControls({ position: { value: [-7, -3.5, -10], step: 0.1 } })}*/>
-              <Cloud opacity={0.5} speed={0.25} width={10} depth={2.5} segments={20} />
-            </group>
-          </>
-        )}
-      </group>
-
-      <Environment files='/3d/christmas_photo_studio_04_1k.hdr' />
-    </>
-  );
-};
