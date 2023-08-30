38e30f8ce60c1c62b692cc59188e91ecbbf3fb2c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-30 18:30:37 +0300

    add room component

--- src/components/threejs/index.js
+++ src/components/threejs/index.js
@@ -1,6 +1,5 @@
 import { Experience } from './Experience';
-import { Bloom } from './Bloom';
-import { Effects } from './Effects';
+import { Bloom, Effects } from './Effects';
 import { Camera } from './Camera';
 import { Extend } from './Extend';
 
@@ -18,10 +17,11 @@ import { Emissives } from './objects/Emissives';
 import { Guitar } from './objects/Guitar';
 import { PetBed } from './objects/PetBed';
 import { Room } from './objects/Room';
+import { Scene } from './objects/Scene';
 import { Tardis } from './objects/Tardis';
 import { TVUnit } from './objects/TVUnit';
 
 export {
   Experience, Bloom, Effects, Camera, Extend,
   BakedMesh, BakedMaterial, SteamMaterial,
-  Bookshelf, Chair, CoffeeTable, Couch, Cube, Desk, Emissives, Guitar, PetBed, Room, Tardis, TVUnit, };
+  Bookshelf, Chair, CoffeeTable, Couch, Cube, Desk, Emissives, Guitar, PetBed, Room, Scene, Tardis, TVUnit, };
