96b850291c347b3077dc9db38e64b13374b5fcf8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-30 18:30:21 +0300

    remove children and rename for updated model

--- src/components/threejs/objects/Cube.jsx
+++ src/components/threejs/objects/Cube.jsx
@@ -4,18 +4,17 @@ import { motion } from 'framer-motion-3d';
 
 import { dataStore, isDarkTheme } from '@/utils';
 
-export const Cube = ({ nodes, materials, children, ...props }) => {
+export const Cube = ({ nodes, materials, ...props }) => {
   const { theme } = dataStore((store) => ({ theme: store.theme }));
 
   return (
-    <motion.group name='Cube' position={[0, 2.44, 0]} scale={0.25} {...props}>
-      <mesh name='Cube001' geometry={nodes.Cube001.geometry} material={nodes.Cube001.material}>
-        <meshStandardMaterial color={`#${isDarkTheme(theme) ? 'B63C3C' : 'CF504A'}`} />
-      </mesh>
-      <mesh name='Cube001_1' geometry={nodes.Cube001_1.geometry} material={nodes.Cube001_1.material}>
+    <motion.group name='Cube' position={[0.01, 2.14, -0.01]} scale={0.25} {...props}>
+      <mesh name='Cube_1' geometry={nodes.Cube_1.geometry} material={nodes.Cube_1.material}>
         <meshStandardMaterial color={`#${isDarkTheme(theme) ? 'FFF3FF' : 'FFF3DD'}`} />
       </mesh>
-      {children}
+      <mesh name='Cube_2' geometry={nodes.Cube_2.geometry} material={nodes.Cube_2.material}>
+        <meshStandardMaterial color={`#${isDarkTheme(theme) ? 'B63C3C' : 'CF504A'}`} />
+      </mesh>
     </motion.group>
   );
 };
