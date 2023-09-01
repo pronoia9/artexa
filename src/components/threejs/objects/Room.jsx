64926484ed14ee52bd89e8270d7a887f33e4a7f1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-09-01 18:19:56 +0300

    add group for ...props for scaling responsively

--- src/components/threejs/objects/Room.jsx
+++ src/components/threejs/objects/Room.jsx
@@ -3,24 +3,20 @@
 import { BakedMesh, Bookshelf, Chair, CoffeeTable, Couch, Desk, Guitar, TVUnit, PetBed, Tardis } from '@/components/threejs';
 import { sceneMotion } from '@/utils';
 
-export const Room = ({ nodes, materials }) => {
+export const Room = ({ nodes, materials, ...props }) => {
   return (
-    <BakedMesh
-      name='Room'
-      geometry={nodes.Room.geometry}
-      material={nodes.Room.material}
-      rotation={[Math.PI, 0, Math.PI]}
-      {...sceneMotion.room}
-    >
-      <Bookshelf nodes={nodes} />
-      <Chair nodes={nodes} />
-      <CoffeeTable nodes={nodes} materials={materials} />
-      <Couch nodes={nodes} materials={materials} />
-      <Desk nodes={nodes} />
-      <Guitar nodes={nodes} />
-      <PetBed nodes={nodes} materials={materials} />
-      <Tardis nodes={nodes} materials={materials} />
-      <TVUnit nodes={nodes} />
-    </BakedMesh>
+    <group name='Room_Container' {...props}>
+      <BakedMesh name='Room' geometry={nodes.Room.geometry} material={nodes.Room.material} rotation={[Math.PI, 0, Math.PI]} {...sceneMotion.room}>
+        <Bookshelf nodes={nodes} />
+        <Chair nodes={nodes} />
+        <CoffeeTable nodes={nodes} materials={materials} />
+        <Couch nodes={nodes} materials={materials} />
+        <Desk nodes={nodes} />
+        <Guitar nodes={nodes} />
+        <PetBed nodes={nodes} materials={materials} />
+        <Tardis nodes={nodes} materials={materials} />
+        <TVUnit nodes={nodes} />
+      </BakedMesh>
+    </group>
   );
 };
