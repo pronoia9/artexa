de6fd16800db18804aa327ba77e1fc124fe01540
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-09-01 18:04:03 +0300

    revert to r3f canvas, remove extends

--- src/components/threejs/Experience.jsx
+++ src/components/threejs/Experience.jsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
 import { Preload, ScrollControls, useProgress } from '@react-three/drei';
 import { Leva } from 'leva';
 import { styled } from 'styled-components';
 import { motion } from 'framer-motion';
-import { MotionCanvas } from 'framer-motion-3d';
 
-import { Extend, Scene, Effects } from '@/components/threejs';
+import { Scene, Effects } from '@/components/threejs';
 import { sceneMotion } from '@/utils';
 
 export const Experience = () => {
@@ -16,23 +16,25 @@ export const Experience = () => {
   return (
     <Container {...sceneMotion.container}>
       <Leva collapsed />
-      <MotionCanvas dpr={[1, 2]} gl={{ antialias: true }} flat>
-        <Extend />
+      <Canvas dpr={[1, 2]} gl={{ antialias: true }} flat>
         <Suspense fallback={null}>
           <ScrollControls pages={3}>
-            <Scene />
+            <Scene  />
           </ScrollControls>
           <Effects />
           <Preload all />
         </Suspense>
-      </MotionCanvas>
+      </Canvas>
     </Container>
   );
 };
 
 const Container = styled(motion.div)`
-  &, canvas {
-    width: 100%;
-    height: 100%;
-  }
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
 `;
