233b902f903ee2492711024c2169f8c39ddaff2f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-09-01 19:08:39 +0300

    add group to adjust responsive height of camera

--- src/components/threejs/Camera.jsx
+++ src/components/threejs/Camera.jsx
@@ -2,10 +2,12 @@
 
 import { PerspectiveCamera } from '@react-three/drei';
 
-export const Camera = () => {
+export const Camera = (props) => {
   return (
     <group name='Camera_Container' position={[20.02, 15.24, 20.01]} rotation={[1.24, 0.3, -0.74]}>
-      <PerspectiveCamera name='TrueIsoCam_1' makeDefault={true} far={1000} near={0.1} fov={22.9} rotation={[-Math.PI / 2, 0, 0]} />
+      <group {...props}>
+        <PerspectiveCamera name='TrueIsoCam_1' makeDefault={true} far={1000} near={0.1} fov={22.9} rotation={[-Math.PI / 2, 0, 0]} />
+      </group>
     </group>
   );
 };
