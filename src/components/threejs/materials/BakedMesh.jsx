96a4a0e503d18204d7852b45c3839d02e6ffcff3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-09-02 17:51:22 +0300

    add forwardref

--- src/components/threejs/materials/BakedMesh.jsx
+++ src/components/threejs/materials/BakedMesh.jsx
@@ -1,19 +1,16 @@
 'use client';
 
-import { MeshBasicMaterial } from 'three';
-import { extend } from '@react-three/fiber';
+import { forwardRef } from 'react';
 import { motion } from 'framer-motion-3d';
 
 import { BakedMaterial } from '@/components/threejs';
 import { rngInRange, sceneMotion } from '@/utils';
 
-extend({ MeshBasicMaterial });
-
-export const BakedMesh = ({ showCube, variants, children, ...props }) => {
+export const BakedMesh = forwardRef(function MyInput({ showCube, variants, children, ...props }, ref) {
   return (
-    <motion.mesh {...sceneMotion.bakedMesh(rngInRange(0.1, 0.25), rngInRange(0.1, 0.25), variants)} {...props}>
+    <motion.mesh ref={ref} {...sceneMotion.bakedMesh(rngInRange(0.1, 0.25), rngInRange(0.1, 0.25), variants)} {...props}>
       <BakedMaterial />
       {children}
     </motion.mesh>
   );
-};
+});
