f9d75704ac9cc7af650245b763e1147fd6c9918e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-09-05 20:33:40 +0300

    add wallpaper confetti to macbook screen

--- src/components/threejs/objects/Desk.jsx
+++ src/components/threejs/objects/Desk.jsx
@@ -1,8 +1,17 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { CatmullRomCurve3, MathUtils, Vector3 } from 'three';
+import { extend, useFrame } from '@react-three/fiber';
+import { MeshPortalMaterial } from '@react-three/drei';
+import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
+import { useControls } from 'leva';
 
 import { BakedMesh, SteamMaterial } from '@/components/threejs';
+// import { colors } from '@/styles';
+// import { dataStore } from '@/utils';
+
+extend({ MeshLineGeometry, MeshLineMaterial });
 
 export const Desk = ({ nodes, ...props }) => {
   return (
@@ -90,6 +99,13 @@ const Headset = ({ nodes }) => (
 // TODO: Idea 1 - Selective Bloom Spheres
 // TODO: Idea 2 - Selective Confetti
 const Macbook = ({ nodes, ...props }) => {
+  // const { accent } = dataStore((state) => ({ accent: state.accent }));
+  const { dash, count, radius } = useControls('Wallpaper', {
+    dash: { value: 0.75, min: 0, max: 0.99, step: 0.01 },
+    count: { value: 50, min: 0, max: 200, step: 1 },
+    radius: { value: 50, min: 1, max: 100, step: 1 },
+  });
+
   return (
     <BakedMesh name='Macbook' geometry={nodes.Macbook.geometry} material={nodes.Macbook.material} position={[-0.01, 0.61, 1.11]} {...props}>
       <mesh
@@ -99,7 +115,19 @@ const Macbook = ({ nodes, ...props }) => {
         position={[0.22, 0.38, 0.06]}
         rotation={[1.59, -0.05, 1.87]}
         scale={1.01}
-      />
+      >
+        <MeshPortalMaterial>
+          <group scale={0.01}>
+            <Lines
+              dash={dash}
+              count={count}
+              radius={radius}
+              colors={[[10, 0.5, 2], [1, 2, 10], '#A2CCB6', '#FCEEB5', '#EE786E', '#e0feff']}
+              // colors={[colors[accent].accent1,colors[accent].accent2,colors[accent].accent3,colors[accent].accent4,colors[accent].accent5,colors[accent].accent1]}
+            />
+          </group>
+        </MeshPortalMaterial>
+      </mesh>
     </BakedMesh>
   );
 };
@@ -139,3 +167,31 @@ const PlantS = ({ nodes }) => (
 const RubixCube = ({ nodes }) => (
   <BakedMesh name='RubixCube' geometry={nodes.RubixCube.geometry} material={nodes.RubixCube.material} position={[0.42, 0.27, -1.87]} />
 );
+
+const Lines = ({ dash, count, colors, radius = 50, rand = MathUtils.randFloatSpread }) => {
+  const [lines] = useState(() => {
+    return Array.from({ length: count }, () => {
+      const pos = new Vector3(rand(radius), rand(radius), rand(radius));
+      const points = Array.from({ length: 10 }, () => pos.add(new Vector3(rand(radius), rand(radius), rand(radius))).clone());
+      const curve = new CatmullRomCurve3(points).getPoints(300);
+      return {
+        color: colors[parseInt(colors.length * Math.random())],
+        width: Math.max(radius / 100, (radius / 50) * Math.random()),
+        speed: Math.max(0.1, 1 * Math.random()),
+        curve: curve.flatMap((point) => point.toArray()),
+      };
+    });
+  });
+  return lines.map((props, index) => <Fatline key={index} dash={dash} {...props} />);
+};
+
+const Fatline = ({ curve, width, color, speed, dash }) => {
+  const ref = useRef();
+  useFrame((state, delta) => (ref.current.material.dashOffset -= (delta * speed) / 10));
+  return (
+    <mesh ref={ref}>
+      <meshLineGeometry points={curve} />
+      <meshLineMaterial transparent lineWidth={width * 0.075} color={color} depthWrite={false} dashArray={0.25} dashRatio={dash} toneMapped={false} />
+    </mesh>
+  );
+};
