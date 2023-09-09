bc41f743a4b109a4ad06455c8d90ff39e8af3684
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-09-09 16:49:10 +0300

    change float settings

--- src/components/threejs/objects/Cube.jsx
+++ src/components/threejs/objects/Cube.jsx
@@ -11,7 +11,13 @@ export const Cube = ({ nodes, materials, ...props }) => {
 
   return (
     <motion.group name='Cube' position={[0.01, 2.14, -0.01]} scale={0.25} {...props}>
-      <Float key={`cube-float-${scroll.offset}`} speed={scroll.offset === 0 ? 5 : 0} factor={1}>
+      <Float
+        key={`cube-float-${scroll.offset}`}
+        speed={scroll.offset === 0 ? 5 : 0} // Animation speed, defaults to 1
+        rotationIntensity={0.25} // XYZ rotation intensity, defaults to 1
+        floatIntensity={3} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+        floatingRange={[-0.1, 0.1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+      >
         <mesh name='Cube_1' geometry={nodes.Cube_1.geometry} material={nodes.Cube_1.material}>
           <meshStandardMaterial color={`#${isDarkTheme(theme) ? 'FFF3FF' : 'FFF3DD'}`} />
         </mesh>
