c7d7f26ee5971c0bd2f08acad50ed52b6b15848d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-09-09 17:47:24 +0300

    hide leva panel

--- src/components/threejs/Experience.jsx
+++ src/components/threejs/Experience.jsx
@@ -1,30 +1,37 @@
 'use client';
 
-import { Suspense } from 'react';
+import { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Preload, ScrollControls, useProgress } from '@react-three/drei';
+import { Preload, ScrollControls } from '@react-three/drei';
 import { Leva } from 'leva';
 import { styled } from 'styled-components';
 import { motion } from 'framer-motion';
 
+import { SVGs } from '@/components';
 import { Scene, Effects } from '@/components/threejs';
 import { sceneMotion } from '@/utils';
 
 export const Experience = () => {
+  const scrollRef = useRef();
   // const { active, progress, errors, item, loaded, total } = useProgress();
 
   return (
-    <Container {...sceneMotion.container}>
-      <Leva collapsed />
+    <Container {...sceneMotion.container} $offset={scrollRef.current?.offset}>
+      {/* <SVGs type='down' /> */}
+
       <Canvas dpr={[1, 2]} gl={{ antialias: true }} flat>
         <Suspense fallback={null}>
           <ScrollControls pages={3}>
-            <Scene />
+            <Scene scrollRef={scrollRef} />
           </ScrollControls>
           <Effects />
           <Preload all />
         </Suspense>
       </Canvas>
+
+      <div className='leva-panel'>
+        <Leva collapsed />
+      </div>
     </Container>
   );
 };
@@ -37,4 +44,8 @@ const Container = styled(motion.div)`
   right: 0;
   width: 100%;
   height: 100%;
+
+  .leva-panel {
+    display: ${({ $offset }) => ($offset > 0 ? '' : 'none')};
+  }
 `;
