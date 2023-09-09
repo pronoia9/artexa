b09b2c96bd43d53cd7b39b5924f7a462e2f5c380
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-09-09 18:04:22 +0300

    add init scroll indicator

--- src/components/threejs/Experience.jsx
+++ src/components/threejs/Experience.jsx
@@ -4,12 +4,12 @@ import { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Preload, ScrollControls } from '@react-three/drei';
 import { Leva } from 'leva';
-import { styled } from 'styled-components';
+import { keyframes, styled } from 'styled-components';
 import { motion } from 'framer-motion';
 
 import { SVGs } from '@/components';
 import { Scene, Effects } from '@/components/threejs';
-import { sceneMotion } from '@/utils';
+import { rem, sceneMotion } from '@/utils';
 
 export const Experience = () => {
   const scrollRef = useRef();
@@ -17,7 +17,11 @@ export const Experience = () => {
 
   return (
     <Container {...sceneMotion.container} $offset={scrollRef.current?.offset}>
-      {/* <SVGs type='down' /> */}
+      <div className='down'>
+        <span>
+          <SVGs type='down' />
+        </span>
+      </div>
 
       <Canvas dpr={[1, 2]} gl={{ antialias: true }} flat>
         <Suspense fallback={null}>
@@ -36,6 +40,11 @@ export const Experience = () => {
   );
 };
 
+const bounce = keyframes`
+  0%   { transform: translateY(-4px) }
+  100% { transform: translateY(4px) }
+`;
+
 const Container = styled(motion.div)`
   position: fixed;
   top: 0;
@@ -44,6 +53,25 @@ const Container = styled(motion.div)`
   right: 0;
   width: 100%;
   height: 100%;
+
+  .down {
+    display: ${({ $offset }) => ($offset > 0 ? 'none' : '')};
+    position: absolute;
+    bottom: 5%;
+    left: calc(50% - 1rem);
+    width: 2rem;
+    height: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid var(--c-font-1);
+    border-radius: ${rem(45)};
+
+    svg {
+      width: 1rem;
+      animation: ${bounce} 0.5s ease-in alternate infinite;
+    }
+  }
 
   .leva-panel {
     display: ${({ $offset }) => ($offset > 0 ? '' : 'none')};
