241c4ce98ca8fe2bd4e481b2211bc26454c4c4c0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-25 13:45:51 -0400

    use array with images count + id + index for images

--- src/components/sections/Project/ProjectScreenshots.jsx
+++ src/components/sections/Project/ProjectScreenshots.jsx
@@ -6,17 +6,24 @@ import { motion } from 'framer-motion';
 import { HoverExpand, SectionWrapper } from '@/components';
 import { projectMotion, rem } from '@/utils';
 
-export const ProjectScreenshots = SectionWrapper(({ images }) => {
+export const ProjectScreenshots = SectionWrapper(({ id, imagesCount }) => {
+  if (!imagesCount) return;
+
+  const array = new Array(imagesCount).fill('');
+
   return (
     <Grid className='col-12' {...projectMotion.screenshots.grid}>
-      {images.map((image, index) => (
-        <motion.div key={`project-screenshots-${index}`} className='grid-item' {...projectMotion.screenshots.image}>
-          <a data-fancybox='project' href={image} className='art-a art-portfolio-item-frame'>
-            <img src={image} alt='item' />
-            <HoverExpand />
-          </a>
-        </motion.div>
-      ))}
+      {array.map((_, index) => {
+        const image = `/images/projects/${id}-${index + 1}.jpeg`;
+        return (
+          <motion.div key={`project-screenshots-${index}`} className='grid-item' {...projectMotion.screenshots.image}>
+            <a data-fancybox='project' href={image} className='art-a art-portfolio-item-frame'>
+              <img src={image} alt='item' />
+              <HoverExpand />
+            </a>
+          </motion.div>
+        );
+      })}
     </Grid>
   );
 });
@@ -30,15 +37,29 @@ const Grid = styled(motion.div)`
 
   & > div {
     height: 100%;
-    &:nth-child(1) { grid-area: one; }
-    &:nth-child(2) { grid-area: two; }
-    &:nth-child(3) { grid-area: three; }
-    &:nth-child(4) { grid-area: four; }
-    &:nth-child(5) { grid-area: five; }
-    &:nth-child(6) { grid-area: six; }
+    &:nth-child(1) {
+      grid-area: one;
+    }
+    &:nth-child(2) {
+      grid-area: two;
+    }
+    &:nth-child(3) {
+      grid-area: three;
+    }
+    &:nth-child(4) {
+      grid-area: four;
+    }
+    &:nth-child(5) {
+      grid-area: five;
+    }
+    &:nth-child(6) {
+      grid-area: six;
+    }
 
-    &:nth-child(3), &:nth-child(4) {
-      a, img {
+    &:nth-child(3),
+    &:nth-child(4) {
+      a,
+      img {
         height: 100%;
         width: auto;
       }
@@ -48,7 +69,8 @@ const Grid = styled(motion.div)`
   @media (max-width: ${rem(768)}) {
     grid-template-areas: 'one' 'two' 'three' 'three' 'four' 'four' 'five' 'six';
 
-    a, img {
+    a,
+    img {
       width: 100% !important;
     }
   }
