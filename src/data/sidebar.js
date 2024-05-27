1ea12e2ecd568c7ce700e2f3004d565f5af83606
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-05-27 17:08:02 -0400

    update languages data

--- src/data/sidebar.js
+++ src/data/sidebar.js
@@ -13,12 +13,11 @@ export const sidebar = {
   ],
 
   languages: {
-    rows: 2,
-    cols: 2,
+    cols: 3,
     items: [
       { id: '1', language: 'English', level: 100 },
       { id: '2', language: 'French', level: 50 },
-      { id: '3', language: 'Turkish', level: 50 },
+      // { id: '3', language: 'Turkish', level: 50 },
       { id: '2', language: 'Japanese', level: 10 },
     ],
   },
