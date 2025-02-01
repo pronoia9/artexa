a0435cb617ed51c878bc5e4203b02523b03866e9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2025-02-01 10:31:01 -0500

    finish prices component

--- src/components/sections/Prices/Prices.jsx
+++ src/components/sections/Prices/Prices.jsx
@@ -1,3 +1,15 @@
-export const Prices = () => {
-  return <div>Prices</div>;
-};
+'use client';
+
+import { PricesCard, SectionTitle, SectionWrapper } from '@/components';
+import { prices as data } from '@/data';
+
+export const Prices = SectionWrapper(() => {
+  return (
+    <>
+      <SectionTitle title={data.title} />
+      {data.list.map((price, index) => (
+        <PricesCard key={`prices-${index}-${price?.title}`} {...price} />
+      ))}
+    </>
+  );
+});
