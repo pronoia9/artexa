953ec9d4b24330475e4219a6086bdbc3e0fcac85
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2025-02-01 07:57:33 -0500

    update contact info

--- src/data/contact.js
+++ src/data/contact.js
@@ -2,14 +2,14 @@ export const contact = {
   title: 'Contact Information',
   info: [
     [
-      { title: 'Email', description: 'jayansin9@gmail.com' },
-      { title: 'Github', description: 'pronoia9', link: 'https://github.com/pronoia9' },
-      { title: 'Discord', description: 'pronoia9' },
+      { title: 'Email', description: 'jayansin9@gmail.com', link: "mailto:jayansin9@gmail.com" },
+      { title: '', description: 'jay@ansin.dev', link: "mailto:jay@ansin.dev" },
+      // { title: 'Github', description: 'pronoia9', link: 'https://github.com/pronoia9' },
+      // { title: 'Discord', description: 'pronoia9' },
     ],
     [
-      { title: 'Mobile', description: '+1 470 XXX XX XX' },
-      { title: 'Skype', description: '@jayansin' },
-      { title: 'Address', description: 'Woodstock, GA' },
+      { title: 'Mobile', description: '+1 470 XXX XX XX', link: "tel:4705509545" },
+      { title: 'Location', description: 'Woodstock, GA' },
     ],
   ],
   form: {
