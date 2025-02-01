c45732028ba48fcd74985e56c8a462d764002bc6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2025-02-01 09:08:48 -0500

    fix year

--- src/data/footer.js
+++ src/data/footer.js
@@ -1,4 +1,4 @@
 export const footer = {
   logos: ['/images/footer/friend of the year.png', '/images/footer/I did it.png', '/images/footer/sticker-2.png', '/images/footer/sticker.png'],
-  text: ['© 2023 All Rights Reserved', 'Email: jayansin9@gmail.com'],
+  text: [`© ${new Date().getFullYear()} All rights reserved`, 'Email: jayansin9@gmail.com'],
 };
