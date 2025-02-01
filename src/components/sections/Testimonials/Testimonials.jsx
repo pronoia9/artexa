a6c98fdc8e0cd12dca488f4c52385f382e4752cc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2025-02-01 10:32:08 -0500

    finish testimonials component

--- src/components/sections/Testimonials/Testimonials.jsx
+++ src/components/sections/Testimonials/Testimonials.jsx
@@ -1,3 +1,15 @@
-export const Testimonials = () => {
-  return <div>Testimonials</div>;
-};
+'use client';
+
+import { SectionTitle, SectionWrapper, TestimonialsCard } from '@/components';
+import { testimonials as data } from '@/data';
+
+export const Testimonials = SectionWrapper(() => {
+  return (
+    <>
+      <SectionTitle title={data.title} />
+      {data.list.map((testimonial, index) => (
+        <TestimonialsCard key={`testimonials-${index}-${testimonial?.title}`} {...testimonial} />
+      ))}
+    </>
+  );
+});
