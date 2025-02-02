0ad5c90000392eba7f01283dd5206f77e7da2073
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2025-02-02 05:16:51 -0500

    add init testimonials card content

--- src/components/sections/Testimonials/TestimonialCard.jsx
+++ src/components/sections/Testimonials/TestimonialCard.jsx
@@ -1,3 +1,47 @@
-export const TestimonialCard = ({ name, subtitle, image, description }) => {
-  return <div>TestimonialsCard</div>;
+'use client';
+
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+export const TestimonialCard = ({ name, subtitle, image, description, rating }) => {
+  const img = image || { src: '', alt: '' };
+
+  return (
+    <Container className='art-a art-testimonial'>
+      <Wrapper className='testimonial-body'>
+        <Image className='art-testimonial-face' {...img} />
+        <Name>{name}</Name>
+        <Subtitle className='art-el-suptitle mb-15'>{subtitle}</Subtitle>
+        <Description className='mb-15'>{description}</Description>
+      </Wrapper>
+
+      <Rating className='art-testimonial-footer'>
+        <div className='art-left-side'>
+          <ul className='art-star-rate'>
+            {new Array(rating).fill().map((r, i) => (
+              <li key={`testimonial-${name}-rating-${i}`}>
+                <FontAwesomeIcon icon={faStar} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className='art-right-side'></div>
+      </Rating>
+    </Container>
+  );
 };
+
+const Container = styled.div``;
+
+const Wrapper = styled.div``;
+
+const Image = styled.img``;
+
+const Name = styled.div``;
+
+const Subtitle = styled.div``;
+
+const Description = styled.div``;
+
+const Rating = styled.div``;
