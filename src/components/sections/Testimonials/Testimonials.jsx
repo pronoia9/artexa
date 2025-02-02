be202295794848605b0b77a406411f643619bf51
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2025-02-02 05:05:09 -0500

    add swiper

--- src/components/sections/Testimonials/Testimonials.jsx
+++ src/components/sections/Testimonials/Testimonials.jsx
@@ -1,15 +1,32 @@
 'use client';
 
-import { SectionTitle, SectionWrapper, TestimonialsCard } from '@/components';
+import { SwiperSlide } from 'swiper/react';
+
+import { SectionTitle, SectionWrapper, Swiper, TestimonialCard } from '@/components';
 import { testimonials as data } from '@/data';
 
 export const Testimonials = SectionWrapper(() => {
+  const swiperOptions = {
+    modules: null,
+    spaceBetween: 0,
+    slidesPerView: 5,
+    speed: 500,
+    autoplay: { delay: 2000 },
+    breakpoints: { 320: { slidesPerView: 3 }, 640: { slidesPerView: 4 }, 1200: { slidesPerView: 5 } },
+    navigation: false,
+    pagination: false,
+  };
+
   return (
     <>
       <SectionTitle title={data.title} />
-      {data.list.map((testimonial, index) => (
-        <TestimonialsCard key={`testimonials-${index}-${testimonial?.title}`} {...testimonial} />
-      ))}
+      <Swiper section='testimonials' autoplay={true} swiperOptions={swiperOptions} navigation={false}>
+        {data.list.map((testimonial, index) => (
+          <SwiperSlide key={`testimonial-${index}`}>
+            <TestimonialCard key={`testimonial-${index}-${testimonial?.title}`} {...testimonial} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 });
