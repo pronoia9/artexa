ad1fc2d9e984acb0afdf8e3e2be91a6296343dee
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2025-02-02 06:48:52 -0500

    use ref and state to store max height

--- src/components/sections/Testimonials/Testimonials.jsx
+++ src/components/sections/Testimonials/Testimonials.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { SwiperSlide } from 'swiper/react';
 
@@ -7,24 +8,39 @@ import { SectionTitle, SectionWrapper, Swiper, TestimonialCard } from '@/compone
 import { testimonials as data } from '@/data';
 
 export const Testimonials = SectionWrapper(() => {
+  const cardRefs = useRef([]);
+  const [maxHeight, setMaxHeight] = useState(0);
+
   const swiperOptions = {
     modules: null,
     spaceBetween: 30,
     slidesPerView: 3,
     speed: 1500,
     autoplay: { delay: 5000 },
-    breakpoints: { 320: { slidesPerView: 1 }, 640: { slidesPerView: 2 }},
+    breakpoints: { 320: { slidesPerView: 1 }, 640: { slidesPerView: 2 } },
     navigation: true,
     pagination: true,
   };
 
+  useEffect(() => {
+    if (cardRefs.current.length) {
+      const allHeights = cardRefs.current.map((ref) => ref?.offsetHeight || 0);
+      const max = Math.max(...allHeights);
+      setMaxHeight(max);
+    }
+  }, [cardRefs.current]);
+
   return (
-    <Container>
+    <Container $maxHeight={maxHeight}>
       <SectionTitle title={data.title} />
       <Swiper section='testimonials' autoplay={true} swiperOptions={swiperOptions} navigation={false}>
         {data.list.map((testimonial, index) => (
           <SwiperSlide key={`testimonial-${index}`}>
-            <TestimonialCard key={`testimonial-${index}-${testimonial?.title}`} {...testimonial} />
+            <TestimonialCard
+              ref={(ref) => (cardRefs.current[index] = ref)}
+              key={`testimonial-${index}-${testimonial?.title}`}
+              {...testimonial}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
@@ -33,16 +49,13 @@ export const Testimonials = SectionWrapper(() => {
 });
 
 const Container = styled.div`
-  &,
-  .art-testimonial-slider {
+  &, .art-testimonial-slider {
     height: 100%;
     width: 100%;
     overflow: visible !important;
   }
-  
-  .swiper-wrapper {
-  }
 
-  .swiper-slide {
+  .swiper-slide, .art-testimonial {
+    min-height: ${({ $maxHeight }) => ($maxHeight ? `${$maxHeight}px` : 'auto')};;
   }
 `;
