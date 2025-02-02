34b7058d66760118e7bd6a9552073961f2c3c6dd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2025-02-02 05:06:21 -0500

    add rating to data

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -7,6 +7,7 @@ export const testimonials = {
       image: '',
       description:
         'Working with Jay was a game-changer. She took my rough vision for a website and turned it into a polished, responsive design with seamless functionality. She was communicative, quick to adapt to feedback, and went above and beyond.',
+      rating: 5,
     },
     {
       name: 'M. Ledford',
@@ -14,6 +15,7 @@ export const testimonials = {
       image: '',
       description:
         'Jay is the kind of person who goes above and beyond to support her team. Whether it’s staying late to ensure everything gets done or stepping in to help where needed, she’s always ready to contribute. Her dedication, reliability, and teamwork make a real difference in any work environment.',
+      rating: 5,
     },
     {
       name: 'C. Leung',
@@ -21,6 +23,7 @@ export const testimonials = {
       image: '',
       description:
         'Jay stood out in my class for her curiosity and problem-solving mindset. She wasn’t just learning concepts—she was applying them, often helping other students debug their code. Her ability to quickly grasp new technologies makes her a standout developer.',
+      rating: 5,
     },
     {
       name: 'Y. Tian',
@@ -28,6 +31,7 @@ export const testimonials = {
       image: '',
       description:
         'Jay’s attention to detail and structured approach made a huge impact on our group. When we hit a major bug before our project deadline, she was the one who stayed late, systematically debugging until we found a solution. Her dedication and logical thinking are invaluable.',
+      rating: 5,
     },
   ],
 };
